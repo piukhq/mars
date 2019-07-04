4297f88bd1433d5241f84b15ff49d81f65a43e7f
Karl Sigiscar <ksigiscar@bink.com>
2019-07-04 18:24:12 +0200

    Initial commit

---
+++ binkapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  binkapp
+//
+//  Created by Karl Sigiscar on 04/07/2019.
+//  Copyright © 2019 Bink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
