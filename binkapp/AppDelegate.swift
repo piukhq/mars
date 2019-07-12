a4cb799fb8e2e4c68f18b1fcaa5ccd8fad2863d1
Karl Sigiscar <ksigiscar@bink.com>
2019-07-12 16:15:32 +0100

    Added comment

--- binkapp/AppDelegate.swift
+++ binkapp/AppDelegate.swift
@@ -13,7 +13,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var dataAccess:DataAccessable?
+    var dataAccess:DataAccessable? // FIXME: Inject from Swinject
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
