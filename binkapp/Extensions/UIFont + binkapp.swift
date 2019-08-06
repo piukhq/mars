466cfe6c475c4d8cb30f49f1f849b52eb11488a4
Paul Tiriteu <ptiriteu@bink.com>
2019-08-06 18:34:28 +0300

    added missing files

---
+++ binkapp/Extensions/UIFont + binkapp.swift
@@ -0,0 +1,70 @@
+//
+//  UIFont + binkapp.swift
+//  binkapp
+//
+//  Created by Paul Tiriteu on 06/08/2019.
+//  Copyright © 2019 Bink. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    
+    class var headline: UIFont {
+        return UIFont(name: "NunitoSans-ExtraBold", size: 25.0)!
+    }
+    
+    class var navbarHeaderLine1: UIFont {
+        return UIFont(name: "NunitoSans-ExtraBold", size: 18.0)!
+    }
+    
+    class var subtitle: UIFont {
+        return UIFont(name: "NunitoSans-ExtraBold", size: 18.0)!
+    }
+    
+    class var buttonText: UIFont {
+        return UIFont(name: "NunitoSans-Bold", size: 18.0)!
+    }
+    
+    class var linkTextButtonNormal: UIFont {
+        return UIFont(name: "NunitoSans-SemiBold", size: 18.0)!
+    }
+    
+    class var linkUnderlined: UIFont {
+        return UIFont(name: "NunitoSans-SemiBold", size: 18.0)!
+    }
+    
+    class var textFieldDescription: UIFont {
+        return UIFont(name: "NunitoSans-Regular", size: 18.0)!
+    }
+    
+    class var textFieldInput: UIFont {
+        return UIFont(name: "NunitoSans-Light", size: 18.0)!
+    }
+    
+    class var bodyTextLarge: UIFont {
+        return UIFont(name: "NunitoSans-Light", size: 18.0)!
+    }
+    
+    class var textFieldLabel: UIFont {
+        return UIFont(name: "NunitoSans-ExtraBold", size: 15.0)!
+    }
+    
+    class var navbarHeaderLine2: UIFont {
+        return UIFont(name: "NunitoSans-Regular", size: 15.0)!
+    }
+    
+    class var textFieldExplainer: UIFont {
+        return UIFont(name: "NunitoSans-Regular", size: 14.0)!
+    }
+    
+    class var textFieldError: UIFont {
+        return UIFont(name: "NunitoSans-Regular", size: 14.0)!
+    }
+    
+    class var bodyTextSmall: UIFont {
+        return UIFont(name: "NunitoSans-Light", size: 14.0)!
+    }
+    
+}
