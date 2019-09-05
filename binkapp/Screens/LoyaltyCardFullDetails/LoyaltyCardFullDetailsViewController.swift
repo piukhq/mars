423523a1eca796502ef8952460affe44ad033e5c
Dorin Pop <>
2019-09-05 16:12:30 +0300

    - created the custom alert view to suport hyperlink

--- binkapp/Screens/LoyaltyCardFullDetails/LoyaltyCardFullDetailsViewController.swift
+++ binkapp/Screens/LoyaltyCardFullDetails/LoyaltyCardFullDetailsViewController.swift
@@ -75,9 +75,10 @@ private extension LoyaltyCardFullDetailsViewController {
         let messageString = "We take security very seriously and keeping your personal details safe is very important to us.\nWe are a PCI certified service provider which means that we meet the highest level of security standards set by the payment card industry. This certification requires us to employ a wide range of measures designed to protect the security of your information. We recommend that you secure your app with Touch ID and/or a passcode for an extra layer of security.\nWe carefully check all the parties who we share your personal information with. To find out more you can see our Privacy Policy here."
         let message = NSMutableAttributedString(string: messageString)
         message.addAttribute(.link, value: securityAdnPrivacyLink ?? "", range: NSRange(location: message.length - 5, length: 4))
-        let alert = UIAlertController(title: "Is my Data Secure?", message: "", preferredStyle: .alert)
-        alert.setValue(message, forKey: "attributedMessage")
-        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+
+        let alert = HyperlinkAlertController(title: "Is my Data Secure?", message: message)
+        alert.modalPresentationStyle = .overCurrentContext
+        alert.modalTransitionStyle = .crossDissolve
         present(alert, animated: true, completion: nil)
     }
 }
