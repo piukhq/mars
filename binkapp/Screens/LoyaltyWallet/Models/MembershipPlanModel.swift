18b53cda4ed4ee4d83f3c1172b97240a26144561
Nick Farrant <you@example.com>
2019-09-13 16:26:49 +0100

    Membership plan relationship mapping

--- binkapp/Screens/LoyaltyWallet/Models/MembershipPlanModel.swift
+++ binkapp/Screens/LoyaltyWallet/Models/MembershipPlanModel.swift
@@ -31,6 +31,36 @@ extension MembershipPlanModel: CoreDataMappable, CoreDataIDMappable {
         update(cdObject, \.id, with: id, delta: delta)
         update(cdObject, \.status, with: status, delta: delta)
 
+        if let featureSet = featureSet {
+//            let overrideID = BowserResponse.idWith(override: id)
+            let cdFeatureSet = featureSet.mapToCoreData(context, .update, overrideID: "")
+            update(cdFeatureSet, \.plan, with: cdObject, delta: delta)
+            update(cdObject, \.featureSet, with: cdFeatureSet, delta: delta)
+        } else {
+            update(cdObject, \.featureSet, with: nil, delta: false)
+        }
+
+        images?.forEach { image in
+            let cdImage = image.mapToCoreData(context, .update, overrideID: nil)
+            update(cdImage, \.plan, with: cdObject, delta: delta)
+            cdObject.addImagesObject(cdImage)
+        }
+
+        if let account = account {
+//            let overrideID = BowserResponse.idWith(override: id)
+            let cdAccount = account.mapToCoreData(context, .update, overrideID: "")
+            update(cdAccount, \.membershipPlan, with: cdObject, delta: delta)
+            update(cdObject, \.account, with: cdAccount, delta: delta)
+        } else {
+            update(cdObject, \.account, with: nil, delta: false)
+        }
+
+        balances?.forEach { balance in
+            let cdBalance = balance.mapToCoreData(context, .update, overrideID: nil)
+            update(cdBalance, \.plan, with: cdObject, delta: delta)
+            cdObject.addBalancesObject(cdBalance)
+        }
+
         return cdObject
     }
 }
