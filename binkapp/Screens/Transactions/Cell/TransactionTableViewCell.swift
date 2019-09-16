e2e35b5f1eaee4d8feaf55c1031f3411424dbfa9
Paul Tiriteu <ptiriteu@bink.com>
2019-09-16 11:19:20 +0300

    added date to transaction cell

--- binkapp/Screens/Transactions/Cell/TransactionTableViewCell.swift
+++ binkapp/Screens/Transactions/Cell/TransactionTableViewCell.swift
@@ -23,20 +23,20 @@ class TransactionTableViewCell: UITableViewCell {
     }
     
     func configure(transactionValue: Int, timestamp: Double, prefix: String?, suffix: String?) {
-       if transactionValue < 0 {
-            valueLabel.text = "-" + (prefix ?? "") + "\(transactionValue)" + (suffix ?? "")
+        if transactionValue < 0 {
+            valueLabel.text = "-" + (prefix ?? "") + "\(abs(transactionValue))" + (suffix ?? "")
             valueLabel.textColor = .black
             transactionImageView.image = UIImage(named: "down")
         } else if transactionValue > 0 {
             valueLabel.text = "+" + (prefix ?? "") + "\(transactionValue)" + (suffix ?? "")
             valueLabel.textColor = .greenOk
             transactionImageView.image = UIImage(named: "up")
-       } else {
+        } else {
             valueLabel.text = (prefix ?? "") + "\(transactionValue)" + (suffix ?? "")
             valueLabel.textColor = .amber
         }
-        
-        
+        let timestampDate = Date(timeIntervalSince1970: timestamp)
+        descriptionLabel.text = timestampDate.getFormattedString(format: "dd MMMM YYYY")
     }
     
     func configureUI() {
