557e799db1234283394bcb3a7404e6e2885c8c4b
Nick Farrant <you@example.com>
2019-09-24 13:08:18 +0100

    Added full payment card model

--- binkapp/Screens/LoyaltyWallet/Models/PaymentCardModel.swift
+++ binkapp/Screens/LoyaltyWallet/Models/PaymentCardModel.swift
@@ -8,17 +8,71 @@
 
 import Foundation
 
-struct PaymentCardModel: Codable {
+struct PaymentCardMembershipCardResponse: Codable {
     var id: Int?
-    var activeLink: Bool?
-    
+    var activeLink: Bool
+
     enum CodingKeys: String, CodingKey {
-        
+        case id
         case activeLink = "active_link"
     }
-    
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        activeLink = try values.decodeIfPresent(Bool.self, forKey: .activeLink)
+}
+
+struct PaymentCardAccountResponse: Codable {
+    var verificationInProgress: Bool?
+    var status: Int?
+    var consents: [PaymentCardAccountConsentsResponse]?
+
+    enum CodingKeys: String, CodingKey {
+        case verificationInProgress = "verification_in_progress"
+        case status
+        case consents
+    }
+}
+
+struct PaymentCardAccountConsentsResponse: Codable {
+    var type: Int?
+    var timestamp: Int?
+}
+
+struct PaymentCardCardResponse: Codable {
+    var firstSix: String?
+    var lastFour: String?
+    var month: Int?
+    var year: Int?
+    var country: String?
+    var currencyCode: String?
+    var nameOnCard: String?
+    var provider: String?
+    var type: String?
+
+    enum CodingKeys: String, CodingKey {
+        case firstSix = "first_six_digits"
+        case lastFour = "last_four_digits"
+        case month
+        case year
+        case country
+        case currencyCode = "currency_code"
+        case nameOnCard = "name_on_card"
+        case provider
+        case type
+    }
+}
+
+struct PaymentCardModel: Codable {
+    var id: Int?
+    var membershipCards: [PaymentCardMembershipCardResponse]?
+    var status: String?
+    var card: PaymentCardCardResponse?
+    var images: [MembershipCardImageModel]?
+    var account: PaymentCardAccountResponse?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case membershipCards = "membership_cards"
+        case status
+        case card
+        case images
+        case account
     }
 }
