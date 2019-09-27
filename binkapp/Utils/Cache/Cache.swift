72a5612c0dc417aeee72ae0661884b3d2b861a0f
Nick Farrant <you@example.com>
2019-09-27 16:48:58 +0100

    Gradient cache util

---
+++ binkapp/Utils/Cache/Cache.swift
@@ -0,0 +1,20 @@
+//
+//  Cache.swift
+//  binkapp
+//
+//  Created by Nick Farrant on 27/09/2019.
+//  Copyright © 2019 Bink. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Cache {
+
+    static let sharedGradientCache = NSCache<NSString, CAGradientLayer>()
+
+    static func clearAll() {
+        Cache.sharedGradientCache.removeAllObjects()
+    }
+
+}
