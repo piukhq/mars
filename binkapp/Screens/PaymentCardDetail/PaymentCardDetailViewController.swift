9335b4aef74593619ad60782142618eb00482b07
Nick Farrant <you@example.com>
2019-10-03 17:38:40 +0100

    Layout WIP

--- binkapp/Screens/PaymentCardDetail/PaymentCardDetailViewController.swift
+++ binkapp/Screens/PaymentCardDetail/PaymentCardDetailViewController.swift
@@ -11,6 +11,33 @@ import UIKit
 class PaymentCardDetailViewController: UIViewController {
     private let viewModel: PaymentCardDetailViewModel
 
+    lazy var stackScrollView: StackScrollView = {
+        let stackView = StackScrollView(axis: .vertical, arrangedSubviews: [titleLabel, descriptionLabel], adjustForKeyboard: true)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .white
+        stackView.margin = UIEdgeInsets(top: 0, left: 25, bottom: 25, right: 0)
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 140, right: 0)
+        view.addSubview(stackView)
+        return stackView
+    }()
+
+    lazy var titleLabel: UILabel = {
+        let title = UILabel()
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.font = UIFont.headline
+        return title
+    }()
+
+    lazy var descriptionLabel: UILabel = {
+        let description = UILabel()
+        description.translatesAutoresizingMaskIntoConstraints = false
+        description.font = UIFont.bodyTextLarge
+        description.numberOfLines = 0
+        return description
+    }()
+
     init(viewModel: PaymentCardDetailViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -22,6 +49,16 @@ class PaymentCardDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        titleLabel.text = "balls"
+        descriptionLabel.text = "hello, wordl"
+
+        NSLayoutConstraint.activate([
+            stackScrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            stackScrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackScrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
+        ])
     }
 
 }
