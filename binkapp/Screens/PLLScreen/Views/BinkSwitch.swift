8022f1eabd125787ddd7065e6eabd1856862737f
Dorin Pop <>
2019-10-04 16:56:36 +0300

    -gradient switch WIP

---
+++ binkapp/Screens/PLLScreen/Views/BinkSwitch.swift
@@ -0,0 +1,33 @@
+//
+//  BinkSwitch.swift
+//  binkapp
+//
+//  Created by Dorin Pop on 04/10/2019.
+//  Copyright © 2019 Bink. All rights reserved.
+//
+
+import UIKit
+
+class BinkSwitch: UISwitch {
+    
+//    override var onTintColor: UIColor? {
+//        didSet {
+//            onTintColor = gradientColor()
+//        }
+//    }
+//
+//    func gradientColor() -> UIColor? {
+//        let gradient = CAGradientLayer()
+//        gradient.frame = frame
+//        let colors = [UIColor.binkPurple.cgColor, UIColor.blueAccent.cgColor]
+//        gradient.colors = colors
+//
+//        UIGraphicsBeginImageContext(gradient.bounds.size)
+//        gradient.render(in: UIGraphicsGetCurrentContext()!)
+//        let bacgroundimage = UIGraphicsGetImageFromCurrentImageContext()!
+//        UIGraphicsEndImageContext()
+//        layer.insertSublayer(gradient, at: 0)
+//
+//        return UIColor(patternImage: bacgroundimage)
+//    }
+}
--- binkapp/Screens/PLLScreen/Views/PaymentCardCell.swift
+++ binkapp/Screens/PLLScreen/Views/PaymentCardCell.swift
@@ -19,27 +19,27 @@ class PaymentCardCell: UITableViewCell {
 //        paymentCardImageView.image = paymentCard.
 //        titleLabel.text = title
 //        subtitleLabel.text = subtitle
-        let binkGradientColor = colorWithGradient(frame: switchButton.frame, colors: [.binkPurple, .blueAccent])
-        switchButton.onTintColor = binkGradientColor
-        switchButton.tintColor = .gray//
+//        let binkGradientColor = colorWithGradient(frame: switchButton.frame, colors: [.binkPurple, .blueAccent])
+//        switchButton.onTintColor = binkGradientColor
+//        switchButton.tintColor = .gray//
     }
     
-    func colorWithGradient(frame: CGRect, colors: [UIColor]) -> UIColor {
-        
-        // create the background layer that will hold the gradient
-        let backgroundGradientLayer = CAGradientLayer()
-        backgroundGradientLayer.frame = frame
-         
-        // we create an array of CG colors from out UIColor array
-        let cgColors = colors.map({$0.cgColor})
-        
-        backgroundGradientLayer.colors = cgColors
-        
-        UIGraphicsBeginImageContext(backgroundGradientLayer.bounds.size)
-        backgroundGradientLayer.render(in: UIGraphicsGetCurrentContext()!)
-        let backgroundColorImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
-        return UIColor(patternImage: backgroundColorImage)
-    }
+//    func colorWithGradient(frame: CGRect, colors: [UIColor]) -> UIColor {
+//        
+//        // create the background layer that will hold the gradient
+//        let backgroundGradientLayer = CAGradientLayer()
+//        backgroundGradientLayer.frame = frame
+//         
+//        // we create an array of CG colors from out UIColor array
+//        let cgColors = colors.map({$0.cgColor})
+//        
+//        backgroundGradientLayer.colors = cgColors
+//        
+//        UIGraphicsBeginImageContext(backgroundGradientLayer.bounds.size)
+//        backgroundGradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+//        let backgroundColorImage = UIGraphicsGetImageFromCurrentImageContext()!
+//        UIGraphicsEndImageContext()
+//        
+//        return UIColor(patternImage: backgroundColorImage)
+//    }
 }
