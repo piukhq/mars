1330df4a573f92eb49e15256ab29d8238ead987c
Nick Farrant <nfarrant@bink.com>
2019-10-11 17:40:16 +0100

    WalletViewController base class wip

--- binkapp/Screens/PaymentWallet/PaymentWalletViewController.swift
+++ binkapp/Screens/PaymentWallet/PaymentWalletViewController.swift
@@ -8,92 +8,36 @@
 
 import UIKit
 
-class PaymentWalletViewController: UIViewController {
-    @IBOutlet weak var collectionView: UICollectionView!
-
-    private let refreshControl = UIRefreshControl()
-    private let viewModel: PaymentWalletViewModel
-    
-    init(viewModel: PaymentWalletViewModel) {
-        self.viewModel = viewModel
-        super.init(nibName: "PaymentWalletViewController", bundle: Bundle(for: PaymentWalletViewController.self))
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+class PaymentWalletViewController: WalletViewController<PaymentWalletViewModel> {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        NotificationCenter.default.addObserver(self, selector: #selector(refresh), name: .didLoadWallet, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(reloadWallet), name: .didAddPaymentCard, object: nil)
-
-        refreshControl.addTarget(self, action: #selector(reloadWallet), for: .valueChanged)
-        collectionView.addSubview(refreshControl)
-        collectionView.alwaysBounceVertical = true
-        
-        collectionView.dataSource = self
-        collectionView.delegate = self
-
-        configureCollectionView()
     }
 
-    private func configureCollectionView() {
+    override func configureCollectionView() {
+        super.configureCollectionView()
+
         collectionView.register(PaymentCardCollectionViewCell.self, asNib: true)
-        collectionView.contentInset = LayoutHelper.WalletDimensions.contentInset
-        guard let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {
-            return
-        }
-        flowLayout.minimumLineSpacing = LayoutHelper.WalletDimensions.cardLineSpacing
     }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        refreshControl.endRefreshing()
-        super.viewWillDisappear(animated)
-    }
-
-    @objc private func reloadWallet() {
-        viewModel.reloadWallet()
-    }
-
-    @objc private func refresh() {
-        refreshControl.endRefreshing()
-        collectionView.reloadData()
-    }
-    
-}
-
-extension PaymentWalletViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.cardCount
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: PaymentCardCollectionViewCell = collectionView.dequeue(indexPath: indexPath)
 
         guard let paymentCard = viewModel.card(forIndexPath: indexPath) else {
             return cell
         }
-        
+
         let cellViewModel = PaymentCardCellViewModel(paymentCard: paymentCard, router: viewModel.router)
         cell.configureWithViewModel(cellViewModel)
 
         return cell
     }
 
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let paymentCard = viewModel.card(forIndexPath: indexPath) else {
             return
         }
         viewModel.toPaymentCardDetail(for: paymentCard)
     }
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return LayoutHelper.WalletDimensions.cardSize
-    }
 }
--- binkapp/Wallet/WalletCard.swift
+++ binkapp/Wallet/WalletCard.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Bink. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import CoreData
 
 enum WalletCardType {
@@ -20,4 +20,78 @@ typealias WalletCard = WalletCardProtocol & NSManagedObject
 protocol WalletCardProtocol {
     var id: String! { get }
     var type: WalletCardType { get }
+}
+
+class WalletViewController<T: WalletViewModel>: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    let refreshControl = UIRefreshControl()
+
+    let viewModel: T
+
+    init(viewModel: T) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NotificationCenter.default.addObserver(self, selector: #selector(refresh), name: .didLoadWallet, object: nil)
+
+        refreshControl.addTarget(self, action: #selector(reloadWallet), for: .valueChanged)
+        collectionView.addSubview(refreshControl)
+        collectionView.alwaysBounceVertical = true
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+        configureCollectionView()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        refreshControl.endRefreshing()
+        super.viewWillDisappear(animated)
+    }
+
+    func configureCollectionView() {
+        collectionView.contentInset = LayoutHelper.WalletDimensions.contentInset
+        guard let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {
+            return
+        }
+        flowLayout.minimumLineSpacing = LayoutHelper.WalletDimensions.cardLineSpacing
+    }
+
+    @objc private func reloadWallet() {
+        viewModel.reloadWallet()
+    }
+
+    @objc private func refresh() {
+        refreshControl.endRefreshing()
+        collectionView.reloadData()
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.cardCount
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return LayoutHelper.WalletDimensions.cardSize
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        fatalError("Should be implemented by subclass")
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        fatalError("Should be implemented by subclass")
+    }
 }
