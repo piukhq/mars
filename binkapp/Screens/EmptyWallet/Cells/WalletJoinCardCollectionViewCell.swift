d95efeea1ec8b5c8b0cf61bcd5f678c6909ece54
Nick Farrant <nfarrant@bink.com>
2019-10-11 10:57:16 +0100

    Cell wip

--- binkapp/Screens/EmptyWallet/Cells/WalletJoinCardCollectionViewCell.swift
+++ binkapp/Screens/EmptyWallet/Cells/WalletJoinCardCollectionViewCell.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class WalletJoinCardCollectionViewCell: UICollectionViewCell {
+    @IBOutlet private weak var brandIconImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var detailLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
