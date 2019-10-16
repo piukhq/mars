41ed0210675f9399daa688dd1859c0142e309da5
Nick Farrant <nfarrant@bink.com>
2019-10-16 10:12:40 +0100

    Stablisting project

--- binkappTests/binkappTests.swift
+++ binkappTests/binkappTests.swift
@@ -7,34 +7,16 @@
 //
 
 import XCTest
-@testable import binkapp
+@testable import binkapp_α
 
 class binkappTests: XCTestCase {
 //    let tabBarTests = TabBarTests()
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-//        tabBarTests.setUp()
+
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
-
-    func testRegister() {
-
+        
     }
 }
