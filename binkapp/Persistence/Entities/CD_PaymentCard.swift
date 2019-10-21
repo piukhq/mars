1febd49c7ff9739509db820b720a37d51384251d
Dorin Pop <dorin.pop@greppysystems.com>
2019-10-21 16:51:27 +0300

    -reverted change for payment card class.

--- binkapp/Persistence/Entities/CD_PaymentCard.swift
+++ binkapp/Persistence/Entities/CD_PaymentCard.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 @objc(CD_PaymentCard)
-open class CD_PaymentCard: _CD_PaymentCard {
+open class CD_PaymentCard: _CD_PaymentCard, WalletCardProtocol {
     // Custom logic goes here.
     
     var imagesArray: [CD_MembershipCardImage] {
