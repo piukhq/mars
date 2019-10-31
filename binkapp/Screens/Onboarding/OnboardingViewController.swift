8fd127dd1342cee0cd39e3cc68d9fabe466043cf
Nick Farrant <nfarrant@bink.com>
2019-10-31 16:18:15 +0000

    Refactoring

--- binkapp/Screens/Onboarding/OnboardingViewController.swift
+++ binkapp/Screens/Onboarding/OnboardingViewController.swift
@@ -11,58 +11,53 @@ import UIKit
 class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet private weak var facebookPillButton: BinkPillButton!
     @IBOutlet private weak var floatingButtonsView: BinkPrimarySecondaryButtonView!
+    private let viewModel: OnboardingViewModel
+    private var didLayoutSubviews = false
+    private var timer: Timer!
 
-    lazy var view1: OnboardingLearningView = {
+    lazy var learningContainer: UIView = {
+        let container = UIView()
+        view.addSubview(container)
+        container.translatesAutoresizingMaskIntoConstraints = false
+        return container
+    }()
+
+    lazy var onboardingView1: OnboardingLearningView = {
         let onboardingView = OnboardingLearningView(frame: .zero)
         onboardingView.configure(forType: .pll)
         return onboardingView
     }()
-    lazy var view2: OnboardingLearningView = {
+
+    lazy var onboardingView2: OnboardingLearningView = {
         let onboardingView = OnboardingLearningView(frame: .zero)
         onboardingView.configure(forType: .wallet)
         return onboardingView
     }()
-    lazy var view3: OnboardingLearningView = {
+
+    lazy var onboardingView3: OnboardingLearningView = {
         let onboardingView = OnboardingLearningView(frame: .zero)
         onboardingView.configure(forType: .barcodeOrCollect)
         return onboardingView
     }()
 
-    lazy var views: [UIView] = {
+    lazy var onboardingViews: [UIView] = {
         return [
-            view1,
-            view2,
-            view3
+            onboardingView1,
+            onboardingView2,
+            onboardingView3
         ]
     }()
 
-    var timer: Timer!
-
-    @objc func fireTimer() {
-        scrollToNext()
-    }
-
-    func scrollToNext() {
-        if pageControl.currentPage != views.count - 1 {
-            let nextPageX: CGFloat = CGFloat(pageControl.currentPage + 1) * scrollView.frame.width
-            pageControl.currentPage = pageControl.currentPage + 1
-            scrollView.setContentOffset(CGPoint(x: nextPageX, y: 0), animated: true)
-        } else {
-            pageControl.currentPage = 0
-            scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
-        }
-    }
-
-    lazy var learningContainer: UIView = {
-        let container = UIView()
-        return container
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView(frame: .zero)
+        learningContainer.addSubview(scrollView)
+        scrollView.bounces = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.delegate = self
+        scrollView.isPagingEnabled = true
+        return scrollView
     }()
-
-    private let scrollView = UIScrollView(frame: .zero)
-
-    private let viewModel: OnboardingViewModel
-
-    private var didLayoutSubviews = false
 
     lazy var pageControl: UIPageControl = {
         let pageControl = UIPageControl()
@@ -72,6 +67,7 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         pageControl.numberOfPages = 3
         pageControl.currentPage = 0
         pageControl.addTarget(self, action: #selector(changePage(pageControl:)), for: .valueChanged)
+        learningContainer.addSubview(pageControl)
         return pageControl
     }()
 
@@ -86,36 +82,24 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 12.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         if !didLayoutSubviews {
             didLayoutSubviews = true
+            setLayout()
             configureUI()
         }
     }
 
-    private func configureUI() {
-        facebookPillButton.configureForType(.facebook)
-        facebookPillButton.addTarget(self, action: #selector(handleFacebookButtonPressed), for: .touchUpInside)
-
-        floatingButtonsView.configure(primaryButtonTitle: viewModel.signUpWithEmailButtonText, secondaryButtonTitle: viewModel.loginWithEmailButtonText)
-        floatingButtonsView.delegate = self
-
-        facebookPillButton.translatesAutoresizingMaskIntoConstraints = false
-        floatingButtonsView.translatesAutoresizingMaskIntoConstraints = false
-        learningContainer.translatesAutoresizingMaskIntoConstraints = false
-
-        view.addSubview(learningContainer)
-
+    private func setLayout() {
         let learningContainerHeightConstraint = learningContainer.heightAnchor.constraint(equalToConstant: 382)
         learningContainerHeightConstraint.priority = .init(999)
 
         NSLayoutConstraint.activate([
-            learningContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            learningContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12),
             learningContainer.leftAnchor.constraint(equalTo: view.leftAnchor),
             learningContainer.rightAnchor.constraint(equalTo: view.rightAnchor),
             learningContainerHeightConstraint,
@@ -128,33 +112,32 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
             facebookPillButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: LayoutHelper.PillButton.widthPercentage),
             facebookPillButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             facebookPillButton.bottomAnchor.constraint(equalTo: floatingButtonsView.topAnchor, constant: -LayoutHelper.PillButton.verticalSpacing),
-        ])
 
-        view.layoutIfNeeded()
-
-        learningContainer.addSubview(scrollView)
-        learningContainer.addSubview(pageControl)
-        scrollView.frame = CGRect(x: 0, y: 12, width: learningContainer.frame.width, height: learningContainer.frame.height-12)
-        scrollView.contentSize = CGSize(width: learningContainer.frame.width * CGFloat(views.count), height: learningContainer.frame.height-12)
-
-        NSLayoutConstraint.activate([
             pageControl.topAnchor.constraint(equalTo: scrollView.bottomAnchor),
             pageControl.bottomAnchor.constraint(lessThanOrEqualTo: facebookPillButton.topAnchor, constant: -25),
             pageControl.heightAnchor.constraint(equalToConstant: 40),
             pageControl.widthAnchor.constraint(equalToConstant: 40),
             pageControl.centerXAnchor.constraint(equalTo: learningContainer.centerXAnchor),
         ])
+    }
 
-        scrollView.bounces = false
-        scrollView.showsVerticalScrollIndicator = false
-        scrollView.showsHorizontalScrollIndicator = false
-        scrollView.delegate = self
+    private func configureUI() {
+        facebookPillButton.translatesAutoresizingMaskIntoConstraints = false
+        floatingButtonsView.translatesAutoresizingMaskIntoConstraints = false
 
-        scrollView.isPagingEnabled = true
+        facebookPillButton.configureForType(.facebook)
+        facebookPillButton.addTarget(self, action: #selector(handleFacebookButtonPressed), for: .touchUpInside)
 
-        for i in 0..<views.count {
-            views[i].frame = CGRect(x: learningContainer.frame.width * CGFloat(i), y: 0, width: learningContainer.frame.width, height: learningContainer.frame.height-12)
-            scrollView.addSubview(views[i])
+        floatingButtonsView.configure(primaryButtonTitle: viewModel.signUpWithEmailButtonText, secondaryButtonTitle: viewModel.loginWithEmailButtonText)
+        floatingButtonsView.delegate = self
+
+        view.layoutIfNeeded() // Get those autolayout fraaaamez
+        scrollView.frame = CGRect(x: 0, y: 0, width: learningContainer.frame.width, height: learningContainer.frame.height)
+        scrollView.contentSize = CGSize(width: learningContainer.frame.width * CGFloat(onboardingViews.count), height: learningContainer.frame.height)
+
+        for i in 0..<onboardingViews.count {
+            onboardingViews[i].frame = CGRect(x: learningContainer.frame.width * CGFloat(i), y: 0, width: learningContainer.frame.width, height: learningContainer.frame.height)
+            scrollView.addSubview(onboardingViews[i])
         }
     }
 
@@ -164,6 +147,8 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         viewModel.notImplemented()
     }
 
+    // MARK: - Scroll view delegate & handlers
+
     @objc func changePage(pageControl: UIPageControl) {
         let x = CGFloat(pageControl.currentPage) * scrollView.frame.size.width
         scrollView.setContentOffset(CGPoint(x: x, y: 0), animated: true)
@@ -172,6 +157,25 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
         pageControl.currentPage = Int(pageNumber)
+    }
+
+    // MARK: Timer
+
+    @objc func fireTimer() {
+        scrollToNext()
+    }
+
+    func scrollToNext() {
+        let nextPage = pageControl.currentPage + 1
+
+        if pageControl.currentPage != onboardingViews.count - 1 {
+            let nextPageX: CGFloat = CGFloat(nextPage) * scrollView.frame.width
+            scrollView.setContentOffset(CGPoint(x: nextPageX, y: 0), animated: true)
+            pageControl.currentPage = nextPage
+        } else {
+            scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+            pageControl.currentPage = 0
+        }
     }
 }
 
@@ -184,4 +188,3 @@ extension OnboardingViewController: BinkPrimarySecondaryButtonViewDelegate {
         viewModel.login()
     }
 }
-
