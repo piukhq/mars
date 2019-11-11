c6e1480ce27564467c70fb9097c09769397237e9
Nick Farrant <nfarrant@bink.com>
2019-11-11 16:35:51 +0000

    Building the nibs

---
+++ binkapp/Screens/PLR/Views/Accumulator/PLRAccumulatorInactiveCell.swift
@@ -0,0 +1,18 @@
+//
+//  PLRAccumulatorInactiveCell.swift
+//  binkapp
+//
+//  Created by Nick Farrant on 11/11/2019.
+//  Copyright © 2019 Bink. All rights reserved.
+//
+
+import UIKit
+
+class PLRAccumulatorInactiveCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ binkapp/Screens/PLR/Views/Stamps/PLRStampsInactiveCell.swift
@@ -0,0 +1,18 @@
+///
+//  PLRStampsInactiveCell.swift
+//  binkapp
+//
+//  Created by Nick Farrant on 11/11/2019.
+//  Copyright © 2019 Bink. All rights reserved.
+//
+
+import UIKit
+
+class PLRStampsInactiveCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
