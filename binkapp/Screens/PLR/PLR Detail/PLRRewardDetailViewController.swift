fb1aebff98437faff0e3432200d2ac1fb913eb45
Nick Farrant <nfarrant@bink.com>
2019-11-15 11:41:45 +0000

    Voucher detail view setup

--- binkapp/Screens/PLR/PLR Detail/PLRRewardDetailViewController.swift
+++ binkapp/Screens/PLR/PLR Detail/PLRRewardDetailViewController.swift
@@ -18,6 +18,7 @@ class PLRRewardDetailViewController: UIViewController {
         stackView.backgroundColor = .clear
         stackView.distribution = .fill
         stackView.alignment = .center
+        stackView.clipsToBounds = false
         stackView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 0, right: 0)
         view.addSubview(stackView)
         return stackView
@@ -27,6 +28,54 @@ class PLRRewardDetailViewController: UIViewController {
         let cell: PLRAccumulatorActiveCell = .fromNib()
         cell.translatesAutoresizingMaskIntoConstraints = false
         return cell
+    }()
+
+    private lazy var codeLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = .headline
+        label.text = "012 345 678 910"
+        return label
+    }()
+
+    private lazy var headerLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = .headline
+        label.text = "Your £5 voucher is ready!"
+        return label
+    }()
+
+    private lazy var subtextLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = .bodyTextLarge
+        label.text = "for joining FatFace Rewards"
+        return label
+    }()
+
+    private lazy var issuedDateLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = .bodyTextSmall
+        label.text = "Issued date"
+        return label
+    }()
+
+    private lazy var redeemedDateLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = .bodyTextSmall
+        label.text = "Redeemed date"
+        return label
+    }()
+
+    private lazy var expiryDateLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = .bodyTextSmall
+        label.text = "Expired date"
+        return label
     }()
 
     // MARK: - Properties
@@ -52,25 +101,28 @@ class PLRRewardDetailViewController: UIViewController {
 
         setupUI()
     }
-
-//    override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//
-//        // This is due to strange layout issues on first appearance
-//        if !hasSetupCell {
-//            hasSetupCell = true
-//            let cardWidth = stackScrollView.frame.width - LayoutHelper.PaymentCardDetail.cardViewPadding
-//            let cardHeight = LayoutHelper.WalletDimensions.cardSize.height
-//            card.frame = CGRect(origin: .zero, size: CGSize(width: cardWidth, height: cardHeight))
-//            card.configureWithViewModel(viewModel.paymentCardCellViewModel, delegate: nil)
-//        }
-//    }
 }
 
 private extension PLRRewardDetailViewController {
     func setupUI() {
         voucher.configureWithViewModel(viewModel.voucherCellViewModel)
-        stackScrollView.add(arrangedSubview: voucher)
+        stackScrollView.add(arrangedSubviews: [voucher, codeLabel, headerLabel, subtextLabel, issuedDateLabel, redeemedDateLabel, expiryDateLabel])
+
+        // View decisioning
+        codeLabel.isHidden = !viewModel.shouldShowCode
+        headerLabel.isHidden = !viewModel.shouldShowHeader
+        subtextLabel.isHidden = !viewModel.shouldShowSubtext
+        issuedDateLabel.isHidden = !viewModel.shouldShowIssuedDate
+        redeemedDateLabel.isHidden = !viewModel.shouldShowRedeemedDate
+        expiryDateLabel.isHidden = !viewModel.shouldShowExpiredDate
+
+        // Values
+        codeLabel.text = viewModel.codeString
+        headerLabel.text = viewModel.headerString
+        subtextLabel.text = viewModel.subtextString
+        issuedDateLabel.text = viewModel.issuedDateString
+        redeemedDateLabel.text = viewModel.redeemedDateString
+        expiryDateLabel.text = viewModel.expiredDateString
 
         setupLayout()
     }
@@ -79,11 +131,17 @@ private extension PLRRewardDetailViewController {
         NSLayoutConstraint.activate([
             stackScrollView.topAnchor.constraint(equalTo: view.topAnchor),
             stackScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            stackScrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            stackScrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            stackScrollView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 25),
+            stackScrollView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -25),
             voucher.topAnchor.constraint(equalTo: stackScrollView.topAnchor),
-            voucher.widthAnchor.constraint(equalTo: stackScrollView.widthAnchor, constant: -50),
+            voucher.widthAnchor.constraint(equalTo: stackScrollView.widthAnchor),
             voucher.heightAnchor.constraint(equalToConstant: 188),
+            codeLabel.widthAnchor.constraint(equalTo: stackScrollView.widthAnchor),
+            headerLabel.widthAnchor.constraint(equalTo: stackScrollView.widthAnchor),
+            subtextLabel.widthAnchor.constraint(equalTo: stackScrollView.widthAnchor),
+            issuedDateLabel.widthAnchor.constraint(equalTo: stackScrollView.widthAnchor),
+            redeemedDateLabel.widthAnchor.constraint(equalTo: stackScrollView.widthAnchor),
+            expiryDateLabel.widthAnchor.constraint(equalTo: stackScrollView.widthAnchor),
         ])
     }
 }
--- binkapp/Screens/PLR/PLR Detail/PLRRewardDetailViewModel.swift
+++ binkapp/Screens/PLR/PLR Detail/PLRRewardDetailViewModel.swift
@@ -18,4 +18,68 @@ class PLRRewardDetailViewModel {
     var voucherCellViewModel: PLRCellViewModel {
         return PLRCellViewModel(voucher: voucher)
     }
+
+    // MARK: - String values
+
+    var codeString: String? {
+        return voucher.code
+    }
+
+    var headerString: String? {
+        return voucher.headline
+    }
+
+    var subtextString: String? {
+        return voucher.subtext
+    }
+
+    var issuedDateString: String? {
+        guard let timestamp = voucher.dateIssued else { return nil }
+        let date = Date(timeIntervalSince1970: timestamp.doubleValue)
+        return date.timeIntervalSince1970.stringFromTimeInterval()
+    }
+
+    var redeemedDateString: String? {
+        guard let timestamp = voucher.dateRedeemed else { return nil }
+        let date = Date(timeIntervalSince1970: timestamp.doubleValue)
+        return date.timeIntervalSince1970.stringFromTimeInterval()
+    }
+
+    var expiredDateString: String? {
+        guard let timestamp = voucher.expiryDate else { return nil }
+        let date = Date(timeIntervalSince1970: timestamp.doubleValue)
+        return date.timeIntervalSince1970.stringFromTimeInterval()
+    }
+
+    // MARK: - View decisioning
+
+    var shouldShowCode: Bool {
+        return voucherState != .inProgress
+    }
+
+    var shouldShowHeader: Bool {
+        return voucherState != .inProgress
+    }
+
+    var shouldShowSubtext: Bool {
+        return false
+    }
+
+    var shouldShowIssuedDate: Bool {
+        return voucherState == .issued || voucherState == .redeemed
+    }
+
+    var shouldShowRedeemedDate: Bool {
+        return voucherState == .redeemed
+    }
+
+    var shouldShowExpiredDate: Bool {
+        return voucherState == .expired
+    }
+
+    // MARK: - Private helpers
+
+    var voucherState: VoucherState? {
+        return VoucherState(rawValue: voucher.state ?? "")
+    }
 }
