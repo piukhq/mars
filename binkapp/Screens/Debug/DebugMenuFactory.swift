12f24ab7da6fd3c57e070f3b953ed03b9432be68
Paul Tiriteu <ptiriteu@bink.com>
2019-12-05 14:46:03 +0200

    displaying current email address

--- binkapp/Screens/Debug/DebugMenuFactory.swift
+++ binkapp/Screens/Debug/DebugMenuFactory.swift
@@ -31,7 +31,8 @@ class DebugMenuFactory {
     }
 
     private func makeEmailAddressRow() -> DebugMenuRow {
-        let currentEmailAddress = Current.userDefaults.string(forKey: "userEmail")
+//        let currentEmailAddress = Current.userDefaults.string(forKey: "userEmail")
+        let currentEmailAddress = Current.userManager.currentEmailAddress
         return DebugMenuRow(title: "Current email address", subtitle: currentEmailAddress, action: {
             self.delegate?.debugMenuFactory(self, shouldPerformActionForType: .email)
         })
