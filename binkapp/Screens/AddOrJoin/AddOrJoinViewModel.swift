1515eeb8358a542a51c10838814afeeb445d5237
Paul Tiriteu <ptiriteu@bink.com>
2020-01-28 14:50:06 +0200

    fixed bug where the button had a white background instead of shadow

--- binkapp/Screens/AddOrJoin/AddOrJoinViewModel.swift
+++ binkapp/Screens/AddOrJoin/AddOrJoinViewModel.swift
@@ -64,7 +64,7 @@ class AddOrJoinViewModel {
             }
         }, showCloseButton: true)
         
-        router.toReusableModalTemplateViewController(configurationModel: configurationModel)
+        router.toReusableModalTemplateViewController(configurationModel: configurationModel, floatingButtons: false)
     }
     
     func brandHeaderWasTapped() {
