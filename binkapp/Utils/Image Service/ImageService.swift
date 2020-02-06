609c0bf7d2ea6ddb46c07b9f81862aba2ec8ff8e
Nick Farrant <nfarrant@bink.com>
2020-02-06 12:24:36 +0000

    HOTFIX: Remove threading work from image service for now

--- binkapp/Utils/Image Service/ImageService.swift
+++ binkapp/Utils/Image Service/ImageService.swift
@@ -24,35 +24,27 @@ final class ImageService {
 
         guard let imagePath = path(forType: pathType) else { return }
 
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            // Are we forcing a refresh?
-            if !forceRefresh {
-                // Is the image in memory?
-                if let cachedImage = Cache.sharedImageCache.object(forKey: imagePath.toNSString()) {
-                    DispatchQueue.main.async {
-                        completion(cachedImage)
-                        return
-                    }
-                }
-
-                // If not, is the image on disk?
-                if let imageFromDisk = try? Disk.retrieve(imagePath, from: .caches, as: UIImage.self) {
-                    DispatchQueue.main.async {
-                        completion(imageFromDisk)
-                    }
-
-                    DispatchQueue.global(qos: .background).async {
-                        // Promote the image to local memory
-                        Cache.sharedImageCache.setObject(imageFromDisk, forKey: imagePath.toNSString())
-                        return
-                    }
-                }
+        // Are we forcing a refresh?
+        if !forceRefresh {
+            // Is the image in memory?
+            if let cachedImage = Cache.sharedImageCache.object(forKey: imagePath.toNSString()) {
+                completion(cachedImage)
+                return
             }
 
-            // Retrieve the image from the API
-            // Either the image is not stored locally, or we are forcing a refresh
-            self?.downloadImage(forPath: imagePath, withPolicy: policy, completion: completion)
+            // If not, is the image on disk?
+            if let imageFromDisk = try? Disk.retrieve(imagePath, from: .caches, as: UIImage.self) {
+                completion(imageFromDisk)
+
+                // Promote the image to local memory
+                Cache.sharedImageCache.setObject(imageFromDisk, forKey: imagePath.toNSString())
+                return
+            }
         }
+
+        // Retrieve the image from the API
+        // Either the image is not stored locally, or we are forcing a refresh
+        downloadImage(forPath: imagePath, withPolicy: policy, completion: completion)
     }
 
     private func path(forType type: PathType) -> String? {
@@ -70,21 +62,17 @@ final class ImageService {
                 return
             }
 
-            DispatchQueue.main.async {
-                completion(downloadedImage)
-            }
+            completion(downloadedImage)
 
-            DispatchQueue.global(qos: .background).async {
-                // Store the downloaded image to disk and fail silently
-                try? Disk.save(downloadedImage, to: .caches, as: path)
+            // Store the downloaded image to disk and fail silently
+            try? Disk.save(downloadedImage, to: .caches, as: path)
 
-                // Store the downloaded image to local memory
-                Cache.sharedImageCache.setObject(downloadedImage, forKey: path.toNSString())
+            // Store the downloaded image to local memory
+            Cache.sharedImageCache.setObject(downloadedImage, forKey: path.toNSString())
 
-                // Add object to sharedStoredObjects, and save to disk
-                let storedObject = StorageUtility.StoredObject(objectPath: path, storedDate: Date(), policy: policy)
-                StorageUtility.addStoredObject(storedObject)
-            }
+            // Add object to sharedStoredObjects, and save to disk
+            let storedObject = StorageUtility.StoredObject(objectPath: path, storedDate: Date(), policy: policy)
+            StorageUtility.addStoredObject(storedObject)
         }
     }
     
