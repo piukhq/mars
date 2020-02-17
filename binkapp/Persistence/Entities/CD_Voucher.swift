f7e036a40721baa68820ce3a3075145198f08d3c
Nick Farrant <nfarrant@bink.com>
2020-02-17 16:56:04 +0000

    Stamps balance string on loyalty card

--- binkapp/Persistence/Entities/CD_Voucher.swift
+++ binkapp/Persistence/Entities/CD_Voucher.swift
@@ -3,7 +3,14 @@ import Foundation
 @objc(CD_Voucher)
 open class CD_Voucher: _CD_Voucher {
     var balanceString: String? {
-        return "\(earn?.prefix ?? "")\(earn?.value?.twoDecimalPointString() ?? "")/\(earn?.prefix ?? "")\(earn?.targetValue?.twoDecimalPointString() ?? "") \(earn?.suffix ?? "")"
+        switch earnType {
+        case .accumulator:
+            return "\(earn?.prefix ?? "")\(earn?.value?.twoDecimalPointString() ?? "")/\(earn?.prefix ?? "")\(earn?.targetValue?.twoDecimalPointString() ?? "") \(earn?.suffix ?? "")"
+        case .stamp:
+            return "\(earn?.value ?? 0)/\(earn?.targetValue ?? 0) \(earn?.suffix?.capitalized ?? "")"
+        default:
+            return nil
+        }
     }
 
     var earnType: VoucherEarnType? {
