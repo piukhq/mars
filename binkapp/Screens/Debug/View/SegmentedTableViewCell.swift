0841e83dba2461b762ef34f5b2c97777ed61cf57
Paul Tiriteu <ptiriteu@bink.com>
2020-02-19 13:40:36 +0200

    added SegmentedTableViewCell to the debug menu

---
+++ binkapp/Screens/Debug/View/SegmentedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SegmentedTableViewCell.swift
+//  binkapp
+//
+//  Created by Paul Tiriteu on 19/02/2020.
+//  Copyright © 2020 Bink. All rights reserved.
+//
+
+import UIKit
+
+class SegmentedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
