891497955ab97900528c923842cf5b3abcca126e
Nick Farrant <nfarrant@bink.com>
2020-02-21 10:55:04 +0000

    Remove BinkTrackableViewController from AnalyticsTrackable protocol

--- binkapp/Screens/BinkUI/BinkTrackableViewController.swift
+++ binkapp/Screens/BinkUI/BinkTrackableViewController.swift
@@ -9,24 +9,8 @@
 import UIKit
 import FirebaseAnalytics
 
-class BinkTrackableViewController: UIViewController, AnalyticsTrackable {
-    /// Optional additional data to pass to analytics along with the default tracking event data
-    var additionalTrackingData: [String: Any]?
-
-    /// Set the trackable event for all subclassing view controllers
-    var trackableEvent: BinkAnalyticsEvent {
-        let className = String(describing: Self.self)
-        return .screenView(screenName: className)
-    }
-    
+class BinkTrackableViewController: UIViewController {
     func setScreenName(trackedScreen: TrackedScreensEnum) {
         Analytics.setScreenName(trackedScreen.rawValue, screenClass: nil)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        /// Disabling in favour of Firebase's out-of-the-box screen name tracking
-//        BinkAnalytics.track(trackableEvent, additionalTrackingData: additionalTrackingData)
     }
 }
