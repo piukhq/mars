4e703bd8a42522c076f9b6287e6ca8fecaf56636
Nick Farrant <nfarrant@bink.com>
2020-03-30 10:18:15 +0100

    WIP

--- binkapp/Screens/LoyaltyScanner/LoyaltyScannerViewController.swift
+++ binkapp/Screens/LoyaltyScanner/LoyaltyScannerViewController.swift
@@ -7,13 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class LoyaltyScannerViewController: UIViewController {
+
+    var session = AVCaptureSession()
+    var captureSchemeOutput: AVCaptureVideoDataOutput?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+
+    var previewLayer = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+
+        session.sessionPreset = .high
+        guard let backCamera = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: backCamera) else { return }
+        performCaptureChecksForDevice(backCamera)
+        captureSchemeOutput = AVCaptureVideoDataOutput()
+
+        if session.canAddInput(input) {
+            session.addInput(input)
+        }
+
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+        guard let videoPreviewLayer = videoPreviewLayer else { return }
+        videoPreviewLayer.videoGravity = .resizeAspect
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+
+        previewLayer.layer.addSublayer(videoPreviewLayer)
+        videoPreviewLayer.frame = view.frame
+    }
+
+    private func performCaptureChecksForDevice(_ device: AVCaptureDevice) {
+        if device.isFocusModeSupported(.continuousAutoFocus) {
+            device.focusMode = .continuousAutoFocus
+        }
+
+        if device.isSmoothAutoFocusSupported {
+            device.isSmoothAutoFocusEnabled = true
+        }
+
+        device.isSubjectAreaChangeMonitoringEnabled = true
+
+        if device.isFocusPointOfInterestSupported {
+            device.focusPointOfInterest = CGPoint(x: 0.5, y: 0.5)
+        }
+
+        if device.isAutoFocusRangeRestrictionSupported {
+            device.autoFocusRangeRestriction = .near
+        }
+
+        if device.isLowLightBoostSupported {
+            device.automaticallyEnablesLowLightBoostWhenAvailable = true
+        }
+
+        device.activeVideoMaxFrameDuration = CMTime(value: 1, timescale: 10)
+        device.activeVideoMinFrameDuration = CMTime(value: 1, timescale: 10)
+        device.unlockForConfiguration()
     }
 
 }
