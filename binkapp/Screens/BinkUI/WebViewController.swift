ae789bc3ea5c20f4c37e755e1dc68c9cdb3940a7
Paul Tiriteu <>
2020-04-28 12:20:04 +0300

    intermediate commit

--- binkapp/Screens/BinkUI/WebViewController.swift
+++ binkapp/Screens/BinkUI/WebViewController.swift
@@ -7,10 +7,18 @@
 //
 
 import UIKit
-import SafariServices
+import WebKit
+
+private struct Constants {
+    static let toolbarHeight: CGFloat = 44
+}
 
 class WebViewController: UIViewController {
+    private var webView: WKWebView!
     private let url: URL?
+    
+    private var backButton: UIBarButtonItem!
+    private var forwardButton: UIBarButtonItem!
     
     init(urlString: String) {
         self.url = URL(string: urlString)
@@ -21,41 +29,97 @@ class WebViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setTopToolbar()
+        setBottomToolbar()
+        
         guard let safeUrl = url else { return }
-        let configuration = SFSafariViewController.Configuration()
-        let viewController = SFSafariViewController(url: safeUrl, configuration: configuration)
-        viewController.delegate = self
-        present(viewController, animated: true)
+        let request = URLRequest(url: safeUrl)
+        webView.load(request)
     }
     
-//    fileprivate func setToolBar() {
-//        let screenWidth = self.view.bounds.width
-//        let backButton = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(goBack))
-//        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 44))
-//        toolBar.isTranslucent = false
-//        toolBar.translatesAutoresizingMaskIntoConstraints = false
-//        toolBar.items = [backButton]
-//        webView.addSubview(toolBar)
-//
-//        NSLayoutConstraint.activate([
-//            toolBar.topAnchor.constraint(equalTo: webView.topAnchor),
-//            toolBar.leadingAnchor.constraint(equalTo: webView.leadingAnchor),
-//            toolBar.trailingAnchor.constraint(equalTo: webView.trailingAnchor)
-//        ])
-//    }
-//
-//    @objc private func goBack() {
-//        if webView.canGoBack {
-//            webView.goBack()
-//        } else {
-//            self.dismiss(animated: true, completion: nil)
-//        }
-//    }
+    private func setTopToolbar() {
+        let screenWidth = self.view.bounds.width
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let closeButton = UIBarButtonItem(image: UIImage(named: "close"), style: .plain, target: self, action: #selector(dismissWebView))
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: screenWidth, height: Constants.toolbarHeight))
+        toolBar.isTranslucent = false
+        toolBar.translatesAutoresizingMaskIntoConstraints = false
+        toolBar.items = [flexibleSpace, closeButton]
+        webView.addSubview(toolBar)
+        
+        // Constraints
+        NSLayoutConstraint.activate([
+            toolBar.topAnchor.constraint(equalTo: webView.topAnchor, constant: 0),
+            toolBar.leadingAnchor.constraint(equalTo: webView.leadingAnchor, constant: 0),
+            toolBar.trailingAnchor.constraint(equalTo: webView.trailingAnchor, constant: 0)
+        ])
+    }
+    
+    private func setBottomToolbar() {
+        let screenWidth = self.view.bounds.width
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+                
+        let backImage = UIImage(named: "forward")?.withHorizontallyFlippedOrientation()
+        let forwardImage = UIImage(named: "forward")
+        
+        backButton = UIBarButtonItem(image: backImage, style: .plain, target: self, action: #selector(goBack))
+        forwardButton = UIBarButtonItem(image: forwardImage, style: .plain, target: self, action: #selector(goForward))
+        let refreshButton = UIBarButtonItem(image: UIImage(named: "refresh"), style: .plain, target: self, action: #selector(refresh))
+        
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: screenWidth, height: Constants.toolbarHeight))
+        toolBar.isTranslucent = false
+        toolBar.translatesAutoresizingMaskIntoConstraints = false
+        toolBar.items = [refreshButton, flexibleSpace, flexibleSpace, flexibleSpace, flexibleSpace, backButton, flexibleSpace, forwardButton]
+        webView.addSubview(toolBar)
+        
+        NSLayoutConstraint.activate([
+            toolBar.bottomAnchor.constraint(equalTo: webView.bottomAnchor, constant: 0),
+            toolBar.leadingAnchor.constraint(equalTo: webView.leadingAnchor, constant: 0),
+            toolBar.trailingAnchor.constraint(equalTo: webView.trailingAnchor, constant: 0)
+        ])
+    }
+    
+    private func checkToolbarItemsState() {
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
+    }
+    
+    @objc private func goBack() {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @objc private func goForward() {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    @objc private func refresh() {
+        webView.reload()
+    }
+
+    @objc private func dismissWebView() {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
 
-extension WebViewController: SFSafariViewControllerDelegate {
-    
+extension WebViewController: WKUIDelegate {}
+
+extension WebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        checkToolbarItemsState()
+    }
 }
