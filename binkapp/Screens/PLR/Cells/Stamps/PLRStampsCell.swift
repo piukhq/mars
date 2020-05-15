3d9510984b7c8ce90576b60c0bc295176bb69ddc
Nick Farrant <nfarrant@bink.com>
2020-05-15 13:12:54 +0100

    Stamps active cell removed stack view and added collection view

--- binkapp/Screens/PLR/Cells/Stamps/PLRStampsCell.swift
+++ binkapp/Screens/PLR/Cells/Stamps/PLRStampsCell.swift
@@ -11,38 +11,38 @@ import UIKit
 class PLRStampsCell: PLRBaseCollectionViewCell {
     struct Constants {
         static let stampViewWidth: CGFloat = 24
+        static let interimSpacing: CGFloat = 12
     }
 
-    @IBOutlet private weak var stampsStackView: UIStackView!
-    
+    @IBOutlet private weak var stampsCollectionView: UICollectionView!
+    private var viewModel: PLRCellViewModel!
+
     override func configureWithViewModel(_ viewModel: PLRCellViewModel) {
         super.configureWithViewModel(viewModel)
-        configureStamps(viewModel: viewModel)
-    }
 
-    private func configureStamps(viewModel: PLRCellViewModel) {
-        for index in 0..<viewModel.stampsAvailable {
-            let stampView = PLRStampView(color: stampColor(atIndex: index, viewModel: viewModel))
-            stampView.translatesAutoresizingMaskIntoConstraints = false
-            stampsStackView.addArrangedSubview(stampView)
-            stampView.widthAnchor.constraint(equalToConstant: Constants.stampViewWidth).isActive = true
+        self.viewModel = viewModel
+        stampsCollectionView.delegate = self
+        stampsCollectionView.dataSource = self
+        stampsCollectionView.register(PLRStampViewCell.self, asNib: true)
+        stampsCollectionView.isScrollEnabled = false
+        if let layout = stampsCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.minimumInteritemSpacing = Constants.interimSpacing
         }
     }
+}
 
-    private func stampColor(atIndex index: Int, viewModel: PLRCellViewModel) -> UIColor {
-        if index < viewModel.stampsCollected {
-            switch viewModel.voucherState {
-            case .redeemed:
-                return .blueAccent
-            case .issued:
-                return .greenOk
-            case .inProgress:
-                return .amberPending
-            default:
-                return .blueInactive
-            }
-        } else {
-            return .black15
-        }
+extension PLRStampsCell: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.stampsAvailable
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: PLRStampViewCell = collectionView.dequeue(indexPath: indexPath)
+        cell.configure(index: indexPath.row, stampsCollected: viewModel.stampsCollected, state: viewModel.voucherState)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: Constants.stampViewWidth, height: Constants.stampViewWidth)
     }
 }
