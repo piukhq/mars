00efe945215857944884de5dcbc6ed07b1f83aa7
Nick Farrant <nfarrant@bink.com>
2020-05-18 17:37:41 +0100

    Nested collection view

--- binkapp/Screens/PLR/Cells/Stamps/PLRStampsCell.swift
+++ binkapp/Screens/PLR/Cells/Stamps/PLRStampsCell.swift
@@ -14,26 +14,30 @@ class PLRStampsCell: PLRBaseCollectionViewCell {
         static let interimSpacing: CGFloat = 12
     }
 
-    @IBOutlet private weak var stampsCollectionView: UICollectionView!
+    @IBOutlet private weak var stampsCollectionView: NestedCollectionView!
     private var viewModel: PLRCellViewModel!
 
     override func configureWithViewModel(_ viewModel: PLRCellViewModel) {
         super.configureWithViewModel(viewModel)
 
         self.viewModel = viewModel
-        stampsCollectionView.delegate = self
-        stampsCollectionView.dataSource = self
         stampsCollectionView.register(PLRStampViewCell.self, asNib: true)
         stampsCollectionView.isScrollEnabled = false
         if let layout = stampsCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
             layout.minimumInteritemSpacing = Constants.interimSpacing
+            stampsCollectionView.translatesAutoresizingMaskIntoConstraints = false
+            stampsCollectionView.dataSource = self
+            stampsCollectionView.isScrollEnabled = false
+            stampsCollectionView.delegate = self
+            stampsCollectionView.backgroundColor = .clear
+            stampsCollectionView.clipsToBounds = false
         }
     }
 }
 
 extension PLRStampsCell: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return viewModel.stampsAvailable
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
