6fa682c74d8acc6fd336bb478adedfabf2a40e01
Nick Farrant <nfarrant@bink.com>
2020-05-20 13:56:39 +0100

    Remove placeholder text

--- binkapp/Screens/Settings/SettingsFactory.swift
+++ binkapp/Screens/Settings/SettingsFactory.swift
@@ -45,7 +45,7 @@ struct SettingsFactory {
                 action: .launchSupport(service: .faq)
             ),
             SettingsRow(
-                title: "\("settings_row_contact_title".localized)\(hasSupportUpdates ? "hasupdates" : "")",
+                title: "settings_row_contact_title".localized,
                 subtitle: "settings_row_contact_subtitle".localized,
                 action: .launchSupport(service: .contactUs),
                 actionRequired: hasSupportUpdates
