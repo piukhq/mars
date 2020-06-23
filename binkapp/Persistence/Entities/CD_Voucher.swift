74d7203a1debe0303c6287f7ddef46646d99e31c
Nick Farrant <nfarrant@bink.com>
2020-06-23 16:37:43 +0100

    Remove duplicated code

--- binkapp/Persistence/Entities/CD_Voucher.swift
+++ binkapp/Persistence/Entities/CD_Voucher.swift
@@ -3,18 +3,14 @@ import Foundation
 @objc(CD_Voucher)
 open class CD_Voucher: _CD_Voucher {
     var balanceString: String? {
+        var suffix = ""
+        if let earnSuffix = earn?.suffix {
+            suffix = " \(earnSuffix)"
+        }
         switch earnType {
         case .accumulator:
-            var suffix = ""
-            if let earnSuffix = earn?.suffix {
-                suffix = " \(earnSuffix)"
-            }
             return "\(earn?.prefix ?? "")\(earn?.value?.twoDecimalPointString() ?? "")/\(earn?.targetValue?.twoDecimalPointString() ?? "")\(suffix)"
         case .stamps:
-            var suffix = ""
-            if let earnSuffix = earn?.suffix {
-                suffix = " \(earnSuffix)"
-            }
             return "\(earn?.prefix ?? "")\(earn?.value ?? 0)/\(earn?.targetValue ?? 0)\(suffix)"
         default:
             return nil
