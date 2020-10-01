83ab8947e74d542516dd702e15870d23b9c9b8ab
Sean Williams <swilliams@bink.com>
2020-10-01 18:00:45 +0100

    Add check for full screen modal presentation

--- binkapp/Screens/BrowseBrands/BrowseBrandsViewController.swift
+++ binkapp/Screens/BrowseBrands/BrowseBrandsViewController.swift
@@ -108,13 +108,17 @@ class BrowseBrandsViewController: BinkTrackableViewController {
         collectionView.register(FilterBrandsCollectionViewCell.self, asNib: true)
         collectionView.delegate = self
         collectionView.dataSource = self
+        var collectionFrameY: CGFloat
         
-        let notchDeviceCollectionFrameY = topStackView.frame.maxY + LayoutHelper.heightForNavigationBar(navigationController?.navigationBar)
-        let nonNotchDeviceCollectionFrameY = topStackView.frame.maxY + LayoutHelper.statusBarHeight
-        let nonModalCollectionFrameY = UIDevice.current.hasNotch ? notchDeviceCollectionFrameY : nonNotchDeviceCollectionFrameY
-        let collectionFrameY = self.isModal ? topStackView.frame.maxY : nonModalCollectionFrameY
-        collectionView.frame = CGRect(x: Constants.marginPadding, y: collectionFrameY, width: view.frame.width - (Constants.marginPadding * 2), height: 0.0)
+        if navigationController?.modalPresentationStyle == .fullScreen {
+            let notchDeviceCollectionFrameY = topStackView.frame.maxY + LayoutHelper.heightForNavigationBar(navigationController?.navigationBar)
+            let nonNotchDeviceCollectionFrameY = topStackView.frame.maxY + LayoutHelper.statusBarHeight
+            collectionFrameY = UIDevice.current.hasNotch ? notchDeviceCollectionFrameY : nonNotchDeviceCollectionFrameY
+        } else {
+            collectionFrameY = topStackView.frame.maxY
+        }
 
+        collectionView.frame = CGRect(x: Constants.marginPadding, y: collectionFrameY, width: view.frame.width - (Constants.marginPadding * 2), height: 0.0)
         view.addSubview(collectionView)
     }
     
