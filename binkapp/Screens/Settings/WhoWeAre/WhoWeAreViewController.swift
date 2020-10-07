f757584f96cfa2cc6735cd9b899dffd0517fd7f2
Sean Williams <swilliams@bink.com>
2020-10-07 11:33:30 +0100

    Add stackview, labels and configure constraints

--- binkapp/Screens/Settings/WhoWeAre/WhoWeAreViewController.swift
+++ binkapp/Screens/Settings/WhoWeAre/WhoWeAreViewController.swift
@@ -8,23 +8,71 @@
 
 import UIKit
 
-class WhoWeAreViewController: UIViewController {
+class WhoWeAreViewController: UIViewController, UIScrollViewDelegate {
 
+    
+    // MARK: - UI Lazy Variables
+
+    private lazy var stackScrollView: StackScrollView = {
+        let stackView = StackScrollView(axis: .vertical, arrangedSubviews: nil, adjustForKeyboard: true)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .white
+        stackView.margin = LayoutHelper.PaymentCardDetail.stackScrollViewMargins
+        stackView.distribution = .fill
+        stackView.alignment = .leading
+        stackView.delegate = self
+        stackView.contentInset = LayoutHelper.PaymentCardDetail.stackScrollViewContentInsets
+        return stackView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.text = "who_we_are_title".localized
+        label.font = UIFont.headline
+        return label
+    }()
+    
+    private lazy var descriptionText: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.text = "who_we_are_body".localized
+        label.font = UIFont.bodyTextLarge
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("HELLO")
+        configureUI()
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+private extension WhoWeAreViewController {
+    func configureUI() {
+        
+        view.addSubview(stackScrollView)
+        stackScrollView.add(arrangedSubview: titleLabel)
+        stackScrollView.add(arrangedSubview: descriptionText)
+        
+        configureLayout()
     }
-    */
+    
+    
+    func configureLayout() {
+        NSLayoutConstraint.activate([
+            stackScrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            stackScrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackScrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
 
+        ])
+    }
+    
 }
