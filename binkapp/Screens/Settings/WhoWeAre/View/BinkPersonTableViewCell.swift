9afc5d9fe2b89e91761a36825aaee559dc354ce4
Sean Williams <swilliams@bink.com>
2020-10-07 15:21:58 +0100

    Add tableView and implement data source methods

---
+++ binkapp/Screens/Settings/WhoWeAre/View/BinkPersonTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  binkPersonTableViewCell.swift
+//  binkapp
+//
+//  Created by Sean Williams on 07/10/2020.
+//  Copyright © 2020 Bink. All rights reserved.
+//
+
+import UIKit
+
+class BinkPersonTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ binkapp/Screens/Settings/WhoWeAreViewController.swift
@@ -0,0 +1,153 @@
+//
+//  WhoWeAreViewController.swift
+//  binkapp
+//
+//  Created by Sean Williams on 07/10/2020.
+//  Copyright © 2020 Bink. All rights reserved.
+//
+
+import UIKit
+
+class WhoWeAreViewController: UIViewController {
+
+    
+    // MARK: - UI Lazy Variables
+
+    private lazy var stackScrollView: StackScrollView = {
+        let stackView = StackScrollView(axis: .vertical, arrangedSubviews: nil, adjustForKeyboard: true)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .white
+        stackView.margin = LayoutHelper.WhoWeAre.stackScrollViewMargins
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.delegate = self
+        stackView.contentInset = LayoutHelper.WhoWeAre.stackScrollViewContentInsets
+        return stackView
+    }()
+    
+    private lazy var binkLogo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "bink-icon-logo")
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 20
+        return imageView
+    }()
+    
+    private lazy var textStackView: StackScrollView = {
+        let stackView = StackScrollView(axis: .vertical)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fill
+        stackView.alignment = .leading
+        stackView.contentInset = LayoutHelper.WhoWeAre.textStackScrollViewContentInsets
+        stackView.isScrollEnabled = false
+        return stackView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.text = "who_we_are_title".localized
+        label.font = UIFont.headline
+        return label
+    }()
+    
+    private lazy var descriptionText: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.text = "who_we_are_body".localized
+        label.font = UIFont.bodyTextLarge
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    var names = ["Sean Williams", "Nick Farrant", "Max Woodhams"]
+
+    private lazy var binkPeopleTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.separatorColor = .lightGray
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(BinkPersonTableViewCell.self, asNib: true)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorInset = LayoutHelper.WhoWeAre.tableViewSeperatorInsets
+        tableView.rowHeight = 80
+        return tableView
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+    }
+}
+
+// MARK: - Tableview Delegates
+
+extension WhoWeAreViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: BinkPersonTableViewCell = binkPeopleTableView.dequeue(indexPath: indexPath)
+        cell.titleLabel.text = names[indexPath.row]
+        
+        return cell
+    }
+    
+ 
+}
+
+
+// MARK: - Private Methods
+
+private extension WhoWeAreViewController {
+    func configureUI() {
+        
+        view.addSubview(stackScrollView)
+        stackScrollView.add(arrangedSubview: binkLogo)
+        textStackView.add(arrangedSubview: titleLabel)
+        textStackView.add(arrangedSubview: descriptionText)
+        stackScrollView.add(arrangedSubview: textStackView)
+        stackScrollView.add(arrangedSubview: binkPeopleTableView)
+        configureLayout()
+
+    }
+    
+    
+    func configureLayout() {
+        NSLayoutConstraint.activate([
+            stackScrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            stackScrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackScrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            binkLogo.heightAnchor.constraint(equalToConstant: 142),
+            binkLogo.widthAnchor.constraint(equalToConstant: 142),
+            textStackView.heightAnchor.constraint(equalToConstant: 200),
+            binkPeopleTableView.heightAnchor.constraint(equalToConstant: 400),
+            binkPeopleTableView.widthAnchor.constraint(equalTo: stackScrollView.widthAnchor)
+        ])
+    }
+    
+}
+
+extension LayoutHelper {
+    struct WhoWeAre {
+        static let stackScrollViewMargins = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 25)
+        static let stackScrollViewContentInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        static let textStackScrollViewContentInsets = UIEdgeInsets(top: 50, left: 0, bottom: 50, right: 0)
+        static let tableViewSeperatorInsets = UIEdgeInsets(top: 0, left: 25, bottom: 0, right: 25)
+
+    }
+}
