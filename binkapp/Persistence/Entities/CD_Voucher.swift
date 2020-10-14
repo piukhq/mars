92a6ac656a040b87973548ce71b8760d13c44838
Nick Farrant <nfarrant@bink.com>
2020-10-14 14:45:18 +0100

    Use computed headline

--- binkapp/Persistence/Entities/CD_Voucher.swift
+++ binkapp/Persistence/Entities/CD_Voucher.swift
@@ -16,6 +16,32 @@ open class CD_Voucher: _CD_Voucher {
             return nil
         }
     }
+    
+    var voucherState: VoucherState? {
+        return VoucherState(rawValue: state ?? "")
+    }
+    
+    var formattedHeadline: String? {
+        let prefix = earn?.prefix ?? ""
+        let targetValue = earn?.targetValue?.intValue ?? 0
+        let value = earn?.value?.intValue ?? 0
+        let displayValue = targetValue - value
+        let displayValueString = "\(displayValue)"
+        
+        var suffix = ""
+        if let earnSuffix = earn?.suffix {
+            suffix = " \(earnSuffix)"
+        }
+        
+        switch (earnType, voucherState) {
+        case (.stamps, .inProgress):
+            return String(format: displayValue > 1 ? "plr_stamp_voucher_headline_plural".localized : "plr_stamp_voucher_headline".localized, prefix, displayValueString)
+        case (.accumulator, .inProgress):
+            return String(format: "plr_accumulator_voucher_headline".localized, prefix, displayValueString, suffix)
+        default:
+            return voucherState?.rawValue.capitalized
+        }
+    }
 
     var earnType: VoucherEarnType? {
         return VoucherEarnType(rawValue: earn?.type ?? "")
