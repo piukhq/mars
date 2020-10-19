052ed214ccbf24af90d266c3e1daec7467886354
Sean Williams <swilliams@bink.com>
2020-10-19 09:07:50 +0100

    Add font to text storage in tap gesture extension

--- binkapp/Screens/BinkUI/HyperlinkLabel.swift
+++ binkapp/Screens/BinkUI/HyperlinkLabel.swift
@@ -11,42 +11,44 @@ import UIKit
 class HyperlinkLabel: UILabel {
     
     private func configure() {
-        
+        self.isUserInteractionEnabled = true
+//        let gesture = UITapGestureRecognizer()
+//        self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapLabel)))
     }
     
     
 }
 
 
-extension UITapGestureRecognizer {
-
-    func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: NSRange) -> Bool {
-        let layoutManager = NSLayoutManager()
-        let textContainer = NSTextContainer(size: CGSize.zero)
-        
-        let mutableAttribString = NSMutableAttributedString(attributedString: label.attributedText!)
-            // Add font so the correct range is returned for multi-line labels
-//        let contactUsRange = (description as NSString).range(of: "Contact us")
-        mutableAttribString.addAttributes([.font: UIFont.linkUnderlined], range: NSRange(location: 0, length: label.attributedText!.length))
-//        mutableAttribString.addAttributes([.underlineStyle : NSUnderlineStyle.single.rawValue, .font: UIFont.linkUnderlined, .foregroundColor: UIColor.blueAccent], range: contactUsRange)
-
-        let textStorage = NSTextStorage(attributedString: mutableAttribString)
-
-        layoutManager.addTextContainer(textContainer)
-        textStorage.addLayoutManager(layoutManager)
-
-        textContainer.lineFragmentPadding = 0.0
-        textContainer.lineBreakMode = label.lineBreakMode
-        textContainer.maximumNumberOfLines = label.numberOfLines
-        let labelSize = label.bounds.size
-        textContainer.size = labelSize
-
-        let locationOfTouchInLabel = self.location(in: label)
-        let textBoundingBox = layoutManager.usedRect(for: textContainer)
-        let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x, y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y)
-        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x, y: locationOfTouchInLabel.y - textContainerOffset.y)
-        let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
-        return NSLocationInRange(indexOfCharacter, targetRange)
-    }
-
-}
+//extension UITapGestureRecognizer {
+//
+//    func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: NSRange) -> Bool {
+//        let layoutManager = NSLayoutManager()
+//        let textContainer = NSTextContainer(size: CGSize.zero)
+//
+//        let mutableAttribString = NSMutableAttributedString(attributedString: label.attributedText!)
+//            // Add font so the correct range is returned for multi-line labels
+////        let contactUsRange = (description as NSString).range(of: "Contact us")
+//        mutableAttribString.addAttributes([.font: UIFont.linkUnderlined], range: NSRange(location: 0, length: label.attributedText!.length))
+////        mutableAttribString.addAttributes([.underlineStyle : NSUnderlineStyle.single.rawValue, .font: UIFont.linkUnderlined, .foregroundColor: UIColor.blueAccent], range: contactUsRange)
+//
+//        let textStorage = NSTextStorage(attributedString: mutableAttribString)
+//
+//        layoutManager.addTextContainer(textContainer)
+//        textStorage.addLayoutManager(layoutManager)
+//
+//        textContainer.lineFragmentPadding = 0.0
+//        textContainer.lineBreakMode = label.lineBreakMode
+//        textContainer.maximumNumberOfLines = label.numberOfLines
+//        let labelSize = label.bounds.size
+//        textContainer.size = labelSize
+//
+//        let locationOfTouchInLabel = self.location(in: label)
+//        let textBoundingBox = layoutManager.usedRect(for: textContainer)
+//        let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x, y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y)
+//        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x, y: locationOfTouchInLabel.y - textContainerOffset.y)
+//        let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+//        return NSLocationInRange(indexOfCharacter, targetRange)
+//    }
+//
+//}
--- binkapp/Screens/PaymentCardDetail/PaymentCardDetailViewController.swift
+++ binkapp/Screens/PaymentCardDetail/PaymentCardDetailViewController.swift
@@ -44,8 +44,8 @@ class PaymentCardDetailViewController: BinkTrackableViewController {
         return title
     }()
 
-    private lazy var addedCardsDescriptionLabel: UILabel = {
-        let description = UILabel()
+    private lazy var addedCardsDescriptionLabel: HyperlinkLabel = {
+        let description = HyperlinkLabel()
         description.font = .bodyTextLarge
         description.numberOfLines = 0
         description.textAlignment = .left
@@ -436,35 +436,35 @@ extension LayoutHelper {
 }
 
 
-//extension UITapGestureRecognizer {
-//
-//    func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: NSRange) -> Bool {
-//        let layoutManager = NSLayoutManager()
-//        let textContainer = NSTextContainer(size: CGSize.zero)
-//        
-//        let mutableAttribString = NSMutableAttributedString(attributedString: label.attributedText!)
-//            // Add font so the correct range is returned for multi-line labels
-////        let contactUsRange = (description as NSString).range(of: "Contact us")
-//        mutableAttribString.addAttributes([.font: UIFont.linkUnderlined], range: NSRange(location: 0, length: label.attributedText!.length))
-////        mutableAttribString.addAttributes([.underlineStyle : NSUnderlineStyle.single.rawValue, .font: UIFont.linkUnderlined, .foregroundColor: UIColor.blueAccent], range: contactUsRange)
-//
-//        let textStorage = NSTextStorage(attributedString: mutableAttribString)
-//
-//        layoutManager.addTextContainer(textContainer)
-//        textStorage.addLayoutManager(layoutManager)
-//
-//        textContainer.lineFragmentPadding = 0.0
-//        textContainer.lineBreakMode = label.lineBreakMode
-//        textContainer.maximumNumberOfLines = label.numberOfLines
-//        let labelSize = label.bounds.size
-//        textContainer.size = labelSize
-//
-//        let locationOfTouchInLabel = self.location(in: label)
-//        let textBoundingBox = layoutManager.usedRect(for: textContainer)
-//        let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x, y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y)
-//        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x, y: locationOfTouchInLabel.y - textContainerOffset.y)
-//        let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
-//        return NSLocationInRange(indexOfCharacter, targetRange)
-//    }
-//
-//}
+extension UITapGestureRecognizer {
+
+    func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: NSRange) -> Bool {
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: CGSize.zero)
+        
+        let mutableAttribString = NSMutableAttributedString(attributedString: label.attributedText!)
+            // Add font so the correct range is returned for multi-line labels
+//        let contactUsRange = (description as NSString).range(of: "Contact us")
+        mutableAttribString.addAttributes([.font: UIFont.bodyTextLarge], range: NSRange(location: 0, length: label.attributedText!.length))
+//        mutableAttribString.addAttributes([.underlineStyle : NSUnderlineStyle.single.rawValue, .font: UIFont.linkUnderlined, .foregroundColor: UIColor.blueAccent], range: contactUsRange)
+
+        let textStorage = NSTextStorage(attributedString: mutableAttribString)
+
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+
+        textContainer.lineFragmentPadding = 0.0
+        textContainer.lineBreakMode = label.lineBreakMode
+        textContainer.maximumNumberOfLines = label.numberOfLines
+        let labelSize = label.bounds.size
+        textContainer.size = labelSize
+
+        let locationOfTouchInLabel = self.location(in: label)
+        let textBoundingBox = layoutManager.usedRect(for: textContainer)
+        let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x, y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y)
+        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x, y: locationOfTouchInLabel.y - textContainerOffset.y)
+        let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+        return NSLocationInRange(indexOfCharacter, targetRange)
+    }
+
+}
