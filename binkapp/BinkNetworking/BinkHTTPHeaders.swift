f122908be6047cc88d3e19fbd353177a627fc479
Sean Williams <swilliams@bink.com>
2020-10-27 10:30:33 +0000

    Remove public keyword from BinkHTTPHeader methods and properties

--- binkapp/BinkNetworking/BinkHTTPHeaders.swift
+++ binkapp/BinkNetworking/BinkHTTPHeaders.swift
@@ -9,52 +9,52 @@
 import UIKit
 
 struct BinkHTTPHeaders {
-    public static func dictionary(_ headers: [BinkHTTPHeader]) -> [String: String] {
+    static func dictionary(_ headers: [BinkHTTPHeader]) -> [String: String] {
         let namesAndValues = headers.map { ($0.name, $0.value) }
         return Dictionary(namesAndValues, uniquingKeysWith: { _, last in last })
     }
 }
 
 struct BinkHTTPHeader {
-    public let name: String
-    public let value: String
+    let name: String
+    let value: String
     
     // MARK: - Headers
 
-    public static func userAgent(_ value: String) -> BinkHTTPHeader {
+    static func userAgent(_ value: String) -> BinkHTTPHeader {
         return BinkHTTPHeader(name: "User-Agent", value: value)
     }
     
-    public static func contentType(_ value: String) -> BinkHTTPHeader {
+    static func contentType(_ value: String) -> BinkHTTPHeader {
         return BinkHTTPHeader(name: "Content-Type", value: value)
     }
     
-    public static func accept(_ value: String) -> BinkHTTPHeader {
+    static func accept(_ value: String) -> BinkHTTPHeader {
         return BinkHTTPHeader(name: "Accept", value: value)
     }
     
-    public static func authorization(_ value: String) -> BinkHTTPHeader {
+    static func authorization(_ value: String) -> BinkHTTPHeader {
         return BinkHTTPHeader(name: "Authorization", value: "Token \(value)")
     }
     
     // MARK: - Defaults
 
-    public static let defaultUserAgent: BinkHTTPHeader = {
+    static let defaultUserAgent: BinkHTTPHeader = {
         return userAgent("Bink App / iOS \(Bundle.shortVersionNumber ?? "") / \(UIDevice.current.systemVersion)")
     }()
     
-    public static let defaultContentType: BinkHTTPHeader = {
+    static let defaultContentType: BinkHTTPHeader = {
         return contentType("application/json")
     }()
     
-    public static let defaultAccept: BinkHTTPHeader = {
+    static let defaultAccept: BinkHTTPHeader = {
         return accept("application/json")
     }()
     
     
     // MARK: - Convert To Dictionary
 
-    public static func dictionary(_ headers: [BinkHTTPHeader]) -> [String: String] {
+    static func dictionary(_ headers: [BinkHTTPHeader]) -> [String: String] {
         let namesAndValues = headers.map { ($0.name, $0.value) }
         return Dictionary(namesAndValues, uniquingKeysWith: { _, last in last })
     }
