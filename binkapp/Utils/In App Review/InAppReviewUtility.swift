4d34f48898bbf5bd9129db35f4a47983ee321cbb
Nick Farrant <nfarrant@bink.com>
2020-11-09 16:14:11 +0000

    Remove redundant protocol conformance

--- binkapp/Utils/In App Review/InAppReviewUtility.swift
+++ binkapp/Utils/In App Review/InAppReviewUtility.swift
@@ -44,7 +44,7 @@ struct TransactionsHistoryInAppReviewableJourney: InAppReviewableJourney {
     typealias J = Self
 }
 
-struct InAppReviewUtility: InAppReviewable {
+struct InAppReviewUtility {
     static let minimumMembershipCards = 4
     static let minimumAppLaunches = 10
     static let minimumDaysSinceFirstLaunch = 2
