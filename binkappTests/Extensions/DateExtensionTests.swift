6351c665d99afa56aee770aca36b25a9210f6d6c
Sean Williams <swilliams@bink.com>
2020-11-12 16:57:42 +0000

    Make date from components test

--- binkappTests/Extensions/DateExtensionTests.swift
+++ binkappTests/Extensions/DateExtensionTests.swift
@@ -11,9 +11,13 @@ import XCTest
 
 class DateExtensionTests: XCTestCase {
     let formatter = DateFormatter()
+    var date: Date!
     
-    override class func setUp() {
+    override func setUp() {
         super.setUp()
+        let isoDate = "2020-11-12T10:44:00+0000"
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        date = formatter.date(from: isoDate)
     }
     
     func test_date_format_enum() {
@@ -24,14 +28,15 @@ class DateExtensionTests: XCTestCase {
     }
     
     func test_getFormattedString_output_format() {
-        let isoDate = "2020-11-12T10:44:00+0000"
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        let date = formatter.date(from: isoDate)
-        
         XCTAssertEqual(date?.getFormattedString(format: .dayMonthYear), "12 November 2020")
         XCTAssertEqual(date?.getFormattedString(format: .dayShortMonthYear), "12 Nov 2020")
         XCTAssertEqual(date?.getFormattedString(format: .dayShortMonthYear24HourSecond), "12 Nov 2020 10:44:00")
         XCTAssertEqual(date?.getFormattedString(format: .dayShortMonthYearWithSlash), "12/11/2020")
+    }
+    
+    func test_make_date_from_components() {
+        let expiryDate = Date.makeDate(year: 2020, month: 11, day: 12, hr: 10, min: 44, sec: 00)
+        XCTAssertEqual(expiryDate, date)
     }
     
     
