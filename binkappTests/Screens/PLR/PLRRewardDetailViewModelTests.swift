0c236130b6c8d49f6ba88a9ff6b342d0636d60ca
Sean Williams <swilliams@bink.com>
2020-11-13 13:59:56 +0000

    Unit test code string property

--- binkappTests/Screens/PLR/PLRRewardDetailViewModelTests.swift
+++ binkappTests/Screens/PLR/PLRRewardDetailViewModelTests.swift
@@ -15,12 +15,17 @@ class PLRRewardDetailViewModelTests: XCTestCase {
     
     override func setUpWithError() throws {
         membershipPlan = MembershipPlanModel(apiId: nil, status: nil, featureSet: nil, images: nil, account: nil, balances: nil, dynamicContent: nil, hasVouchers: true, card: nil)
-        voucher = VoucherModel(apiId: nil, state: nil, code: nil, barcode: nil, barcodeType: nil, headline: nil, subtext: nil, dateRedeemed: nil, dateIssued: nil, expiryDate: nil, earn: nil, burn: nil)
+        voucher = VoucherModel(apiId: nil, state: nil, code: "123456", barcode: nil, barcodeType: nil, headline: nil, subtext: nil, dateRedeemed: nil, dateIssued: nil, expiryDate: nil, earn: nil, burn: nil)
     }
     
     func test_title_string_is_correct() {
         membershipPlan.account = MembershipPlanAccountModel(apiId: nil, planName: nil, planNameCard: nil, planURL: nil, companyName: "Tesco", category: nil, planSummary: nil, planDescription: nil, barcodeRedeemInstructions: nil, planRegisterInfo: nil, companyURL: nil, enrolIncentive: nil, forgottenPasswordUrl: nil, tiers: nil, planDocuments: nil, addFields: nil, authoriseFields: nil, registrationFields: nil, enrolFields: nil)
         let sut = PLRRewardDetailViewModelMock(voucher: voucher, plan: membershipPlan)
         XCTAssertEqual(sut.title, "Tesco")
+    }
+    
+    func test_codeString_is_correct() {
+        let sut = PLRRewardDetailViewModelMock(voucher: voucher, plan: membershipPlan)
+        XCTAssertEqual(sut.codeString, "123456")
     }
 }
