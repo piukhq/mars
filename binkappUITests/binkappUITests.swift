f658028574e931777ef9ba83a806cd50148212d0
Sean Williams <swilliams@bink.com>
2020-11-27 16:48:00 +0000

    Add updated config files and Login test

--- binkappUITests/binkappUITests.swift
+++ binkappUITests/binkappUITests.swift
@@ -7,22 +7,42 @@
 //
 
 import XCTest
-//@testable import binkapp
 
 class binkappUITests: XCTestCase {
     override func setUp() {
         super.setUp()
-
         continueAfterFailure = false
-
-        XCUIApplication().launch()
+        UIView.setAnimationsEnabled(false)
     }
-
-    override func tearDown() {
-        super.tearDown()
+    
+    func test_loginScreenOnAppLaunch_exists() {
+        let app = XCUIApplication()
+        app.launchArguments = ["enable-testing"]
+        app.launch()
+        let buttonsQuery = app.buttons
+        XCTAssertTrue(buttonsQuery.staticTexts["Log in with email"].waitForExistence(timeout: 20), "Login button does not exist")
     }
+    
+    func test_loginWithEmailAndPassword() {
+        let app = XCUIApplication()
+        app.launchArguments = ["enable-testing"]
+        app.launch()
+        
+        let buttonsQuery = app.buttons
+        let loginWithEmailButton = buttonsQuery.staticTexts["Log in with email"]
+        loginWithEmailButton.tap()
+        
+        let emailTextfield = app.textFields.element(boundBy: 0)
+        emailTextfield.tap()
+        emailTextfield.typeText("binklogin@binktest.com")
+        app.buttons["next"].tap()
+        
+        app.typeText("Binklogin123")
+        app.buttons["done"].tap()
+        app.buttons["Continue"].tap()
+        
+        let paymentTabBarButton = app.buttons["Payment"]
 
-    func testExample() {
-        print("TESTING")
+        XCTAssertTrue(paymentTabBarButton.waitForExistence(timeout: 30))
     }
 }
