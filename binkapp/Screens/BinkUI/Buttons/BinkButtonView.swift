4c9b075939e87f5ae0cb958f6f18d622c665e41a
Nick Farrant <nfarrant@bink.com>
2020-12-16 14:29:07 +0000

    WIP BinkButtonView

--- binkapp/Screens/BinkUI/Buttons/BinkButtonView.swift
+++ binkapp/Screens/BinkUI/Buttons/BinkButtonView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 typealias BinkButtonAction = () -> Void
 
-struct BinkButton {
+class BinkButtonConfiguration {
     enum ButtonType {
         case pill(BinkPillButton.PillButtonType)
         case gradient
@@ -19,14 +19,37 @@ struct BinkButton {
 
     let type: ButtonType
     let title: String
-    let action: BinkButtonAction
-    let enabled: Bool = true
+    let enabled: Bool
+    let target: Any?
+    private let action: BinkButtonAction
+
+    init(type: ButtonType, title: String, enabled: Bool = true, target: Any?, action: @escaping BinkButtonAction) {
+        self.type = type
+        self.title = title
+        self.enabled = enabled
+        self.target = target
+        self.action = action
+    }
+
+    @objc func performAction() {
+        action()
+    }
 }
 
 class BinkButtonsView: UIStackView {
-    init(buttons: [BinkButton], addingTo view: UIView) {
-        super.init(frame: .zero)
+    private let buttons: [BinkButtonConfiguration]
 
+    init(buttons: [BinkButtonConfiguration]) {
+        self.buttons = buttons
+        super.init(frame: .zero)
+        configure()
+    }
+
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
         translatesAutoresizingMaskIntoConstraints = false
         axis = .vertical
         distribution = .fill
@@ -34,10 +57,39 @@ class BinkButtonsView: UIStackView {
         spacing = 25
 
         buttons.forEach {
-            switch $0.type {
-            case .pill(let pillButtonType):
-                
-            }
+            let button = makeButton(for: $0)
+            button.heightAnchor.constraint(equalToConstant: 52).isActive = true
+            addArrangedSubview(button)
+        }
+    }
+
+    func attach(to view: UIView) {
+        // Add and configure view
+        view.addSubview(self)
+        NSLayoutConstraint.activate([
+            bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            leftAnchor.constraint(equalTo: view.leftAnchor),
+            rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+    }
+
+    private func makeButton(for configuration: BinkButtonConfiguration) -> UIButton {
+        switch configuration.type {
+        case .pill(let pillButtonType):
+            let button = BinkPillButton()
+            button.configureForType(pillButtonType, hasShadow: true)
+            button.addTarget(configuration.target, action: #selector(configuration.performAction), for: .touchUpInside)
+            return button
+        case .gradient:
+            let button = BinkGradientButton()
+            button.configure(title: configuration.title, hasShadow: true)
+            button.addTarget(configuration.target, action: #selector(configuration.performAction), for: .touchUpInside)
+            return button
+        case .plain:
+            let button = UIButton()
+            button.setTitle(configuration.title, for: .normal)
+            button.addTarget(configuration.target, action: #selector(configuration.performAction), for: .touchUpInside)
+            return button
         }
     }
 }
