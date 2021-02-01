f44ec74575c7aa07cc5ea122874e3c006122b444
Sean Williams <swilliams@bink.com>
2021-02-01 15:20:08 +0000

    Create BinkAlertController and implement dark mode using theme manager

---
+++ binkapp/Screens/BinkUI/BinkAlertController.swift
@@ -0,0 +1,36 @@
+//
+//  BinkAlertController.swift
+//  binkapp
+//
+//  Created by Sean Williams on 01/02/2021.
+//  Copyright © 2021 Bink. All rights reserved.
+//
+
+import UIKit
+
+class BinkAlertController: UIAlertController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureForCurrentTheme()
+        Current.themeManager.addObserver(self, handler: #selector(configureForCurrentTheme))
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        configureForCurrentTheme()
+    }
+    
+    @objc func configureForCurrentTheme() {
+        view.tintColor = Current.themeManager.color(for: .text)
+        
+        let backgroundTransitionView = self.parent
+        
+        switch Current.themeManager.currentTheme.type {
+        case .dark:
+            overrideUserInterfaceStyle = .dark
+        case .light:
+            overrideUserInterfaceStyle = .light
+        case .system:
+            overrideUserInterfaceStyle = .unspecified
+        }
+    }
+}
