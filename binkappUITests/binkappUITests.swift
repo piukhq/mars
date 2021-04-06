75d41414a81ec96bff64c12c7f2625211fec5e83
Sean Williams <swilliams@bink.com>
2021-04-06 16:38:35 +0100

    Implement adding iceland card ui test

--- binkappUITests/binkappUITests.swift
+++ binkappUITests/binkappUITests.swift
@@ -32,7 +32,6 @@ class binkappUITests: XCTestCase {
         emailTextfield.tap()
         emailTextfield.typeText("binklogin@binktest.com")
         app.buttons["next"].tap()
-
         app.typeText("Binklogin123")
         app.buttons["done"].tap()
         app.buttons["Continue"].tap()
@@ -41,11 +40,42 @@ class binkappUITests: XCTestCase {
         XCTAssertTrue(paymentTabBarButton.waitForExistence(timeout: 30))
     }
     
-//    func test_addIcelandCard_isSuccessful() {
-//        let app = UIApplication()
-//        app.launchArguments = ["enable-testing"]
-//        app.launch()
-//        
-//        
-//    }
+    func test_addIcelandCard_isSuccessful() {
+        app.buttons["Browse brands"].tap()
+        app.tables.staticTexts["Iceland"].tap()
+        app.buttons["Add my card"].tap()
+        let cardNumberTextfield = app.textFields["You'll usually find this on the front of your loyalty card."]
+        cardNumberTextfield.tap()
+        cardNumberTextfield.typeText("6332040000200000002")
+        app.buttons["next"].tap()
+        app.typeText("one")
+        app.buttons["next"].tap()
+        app.typeText("rg1 1aa")
+
+        app.toolbars["Toolbar"].buttons["Done"].tap()
+        app.buttons["Add card"].tap()
+        app.buttons["Done"].tap()
+
+        let backButton = app.navigationBars["binkapp.LoyaltyCardFullDetailsView"].buttons["Back"]
+        backButton.tap()
+        sleep(60)
+
+        // Pull to refresh
+        let firstCell = app.staticTexts["Iceland"]
+        let start = firstCell.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
+        let finish = firstCell.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 50))
+        start.press(forDuration: 0, thenDragTo: finish)
+        
+        sleep(15)
+        app.staticTexts["Iceland"].tap()
+
+        XCTAssertTrue(app.staticTexts["£1 "].waitForExistence(timeout: 120))
+        
+        
+//        app.navigationBars["binkapp.TransactionsView"].children(matching: .button).element.tap()
+//        scrollViewsQuery.otherElements.containing(.button, identifier:"Tap card to show barcode").children(matching: .other).element(boundBy: 1).children(matching: .other).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element/*@START_MENU_TOKEN@*/.swipeRight()/*[[".swipeUp()",".swipeRight()"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+//        elementsQuery.tables/*@START_MENU_TOKEN@*/.staticTexts["Delete Card"]/*[[".cells.staticTexts[\"Delete Card\"]",".staticTexts[\"Delete Card\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+//        app.alerts.scrollViews.otherElements.buttons["Yes"].tap()
+        
+    }
 }
