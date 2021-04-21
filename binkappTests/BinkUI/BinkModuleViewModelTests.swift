aa04b00e4a44d71120e825534e3cbbe2837fc647
Nick Farrant <nfarrant@bink.com>
2021-04-21 18:19:00 +0100

    Add unit tests file

---
+++ binkappTests/BinkUI/BinkModuleViewModelTests.swift
@@ -0,0 +1,14 @@
+//
+//  BinkModuleViewModelTests.swift
+//  binkappTests
+//
+//  Created by Nick Farrant on 21/04/2021.
+//  Copyright © 2021 Bink. All rights reserved.
+//
+
+import XCTest
+@testable import binkapp
+
+class BinkModuleViewModelTests: XCTestCase {
+    
+}
