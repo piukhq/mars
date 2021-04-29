83e5fdc7f8c31b86be50219d81f4bb9853c4f141
Sean Williams <swilliams@bink.com>
2021-04-29 12:54:33 +0100

    Bink switch gradient

--- binkapp/Extensions/UIColor+binkapp.swift
+++ binkapp/Extensions/UIColor+binkapp.swift
@@ -95,7 +95,7 @@ extension UIColor {
     static let mastercardPaymentCardGradients: [CGColor] = [UIColor.mastercardGradientLeft.cgColor, UIColor.mastercardGradientRight.cgColor]
     static let amexPaymentCardGradients: [CGColor] = [UIColor.amexGradientLeft.cgColor, UIColor.amexGradientRight.cgColor]
     static let unknownPaymentCardGradients: [CGColor] = [UIColor.unknownGradientLeft.cgColor, UIColor.unknownGradientRight.cgColor]
-    static let binkSwitchGradients: [CGColor] = [UIColor.binkPurple.cgColor, UIColor.blueAccent.cgColor]
+    static let binkSwitchGradients: [CGColor] = [UIColor.binkGradientBlueLeft.cgColor, UIColor.binkGradientBlueRight.cgColor]
 
     // MARK: - Loyalty card secondary color helpers
 
