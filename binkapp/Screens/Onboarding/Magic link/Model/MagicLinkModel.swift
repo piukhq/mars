ebeaf4126c9cc9cbf04a8b4868fc6da4d97a34d7
Nick Farrant <nfarrant@bink.com>
2021-05-04 14:20:54 +0100

    Magic link request and response models

---
+++ binkapp/Screens/Onboarding/Magic link/Model/MagicLinkModel.swift
@@ -0,0 +1,42 @@
+//
+//  MagicLinkModel.swift
+//  binkapp
+//
+//  Created by Nick Farrant on 04/05/2021.
+//  Copyright © 2021 Bink. All rights reserved.
+//
+
+import Foundation
+
+struct MagicLinkRequestModel: Codable {
+    let email: String
+    let slug: String
+    let locale: String
+    let bundleId: String
+    
+    init(email: String) {
+        self.email = email
+        self.slug = "iceland"
+        self.locale = "en_GB"
+        self.bundleId = APIConstants.bundleID
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case email
+        case slug
+        case locale
+        case bundleId = "bundle_id"
+    }
+}
+
+struct MagicLinkAccessTokenRequestModel: Codable {
+    let token: String
+}
+
+struct MagicLinkAccessTokenResponseModel: Codable {
+    let accessToken: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+    }
+}
