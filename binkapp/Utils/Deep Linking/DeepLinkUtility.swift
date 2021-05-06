18d5facdda5575f5a8e7d44da67b07f15f00f0e8
Nick Farrant <nfarrant@bink.com>
2021-05-06 13:43:31 +0100

    More WIP

--- binkapp/Utils/Deep Linking/DeepLinkUtility.swift
+++ binkapp/Utils/Deep Linking/DeepLinkUtility.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class DeepLinkUtility: UserServiceProtocol {
+class DeepLinkUtility {
     enum DeepLinkType: String {
         case magicLink = "magic"
     }
@@ -28,46 +28,52 @@ class DeepLinkUtility: UserServiceProtocol {
         
         for item in queryItems {
             if item.name == "token", let token = item.value {
-                // TODO: Use token to exchange for access token
-                requestMagicLinkAccessToken(for: token) { [weak self] result in
-                    switch result {
-                    case .success(let response):
-                        // TODO: Pass error back to in completion
-                        guard let email = response.email else { return }
-                        
-                        Current.userManager.setNewUser(with: response)
-                        
-                        // TODO: This never gets called?
-                        self?.createService(params: APIConstants.makeServicePostRequest(email: email), completion: { (success, _) in
-                            // TODO: Pass error back in completion
-                            guard success else { return }
-
-                            self?.getUserProfile(completion: { result in
-                                guard let response = try? result.get() else {
-                                    BinkAnalytics.track(OnboardingAnalyticsEvent.end(didSucceed: false))
-                                    return
-                                }
-                                Current.userManager.setProfile(withResponse: response, updateZendeskIdentity: true)
-                                BinkAnalytics.track(OnboardingAnalyticsEvent.userComplete)
-                            })
-
-                            // TODO: Pass success back in completion
-//                            self?.continueButton.toggleLoading(isLoading: false)
-                            Current.rootStateMachine.handleLogin()
-                            BinkAnalytics.track(OnboardingAnalyticsEvent.serviceComplete)
-                            BinkAnalytics.track(OnboardingAnalyticsEvent.end(didSucceed: true))
-                        })
-                    
-                    
-                    case .failure(let error):
-                        print("Handle error: \(error.localizedDescription)")
-                    }
-                }
+                let loginController = LoginController()
+                loginController.exchangeMagicLinkToken(token: token)
             }
         }
     }
 }
 
-class LoginController {
+// TODO: Move this, and bring standard login into it also
+class LoginController: UserServiceProtocol {
+    func exchangeMagicLinkToken(token: String) {
+        requestMagicLinkAccessToken(for: token) { [weak self] result in
+            self?.handleResult(result)
+        }
+    }
     
+    private func handleResult(_ result: Result<LoginResponse, UserServiceError>) {
+        switch result {
+        case .success(let response):
+            // TODO: Pass error back to in completion
+            guard let email = response.email else { return }
+            
+            Current.userManager.setNewUser(with: response)
+            
+            // TODO: This never gets called?
+            createService(params: APIConstants.makeServicePostRequest(email: email), completion: { [weak self] (success, _) in
+                // TODO: Pass error back in completion
+                guard success else { return }
+
+                self?.getUserProfile(completion: { result in
+                    guard let response = try? result.get() else {
+                        BinkAnalytics.track(OnboardingAnalyticsEvent.end(didSucceed: false))
+                        return
+                    }
+                    Current.userManager.setProfile(withResponse: response, updateZendeskIdentity: true)
+                    BinkAnalytics.track(OnboardingAnalyticsEvent.userComplete)
+                })
+
+                // TODO: Pass success back in completion
+//                            self?.continueButton.toggleLoading(isLoading: false)
+                Current.rootStateMachine.handleLogin()
+                BinkAnalytics.track(OnboardingAnalyticsEvent.serviceComplete)
+                BinkAnalytics.track(OnboardingAnalyticsEvent.end(didSucceed: true))
+            })
+        
+        case .failure(let error):
+            print("Handle error: \(error.localizedDescription)")
+        }
+    }
 }
