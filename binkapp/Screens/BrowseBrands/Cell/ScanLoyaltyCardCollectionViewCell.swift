70a78207a49f6669abc17cfdc50faf8ec4f365b6
Sean Williams <swilliams@bink.com>
2021-05-10 14:07:35 +0100

    Create collection view cell. Add image view and labels and constraints.

---
+++ binkapp/Screens/BrowseBrands/Cell/ScanLoyaltyCardCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ScanLoyaltyCardCollectionViewCell.swift
+//  binkapp
+//
+//  Created by Sean Williams on 10/05/2021.
+//  Copyright © 2021 Bink. All rights reserved.
+//
+
+import UIKit
+
+class ScanLoyaltyCardCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
