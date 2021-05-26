989cf50919a2d164ce8bb163345fa5b7a27d7397
Sean Williams <swilliams@bink.com>
2021-05-26 16:58:58 +0100

    Formate H3 tags within H2 formatting loop and after if there are no H2 tags

--- binkapp/Screens/AuthAndAdd/AuthAndAddViewModel.swift
+++ binkapp/Screens/AuthAndAdd/AuthAndAddViewModel.swift
@@ -353,44 +353,82 @@ class AuthAndAddViewModel {
             // First Paragraph
             let firstParagraph = contents.slice(from: "<h1>", to: "<h2>") ?? ""
             if let attributedString = try? NSMutableAttributedString(data: Data(firstParagraph.utf8), options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
-                // Format all text
-                attributedString.addAttribute(.font, value: UIFont.bodyTextLarge, range: NSRange(location: 0, length: attributedString.string.count - 1))
-
-                // Format title
-                let titleString = contents.slice(from: "<h1>", to: "</h1>")
-                let formattedTitle = titleString?.replacingOccurrences(of: "&amp;", with: "&")
-                if let titleRange = attributedString.string.range(of: formattedTitle ?? "") {
-                    let nsTitleRange = NSRange(titleRange, in: attributedString.string)
-                    attributedString.addAttribute(.font, value: UIFont.headline, range: nsTitleRange)
-                }
-
-                mutableAttributedString = attributedString
-                mutableAttributedString.append(newLine)
-            }
-            
-            
-            // Remaining paragraphs
-            let paragraphs = contents.components(separatedBy: "<h2>")
-            for paragraph in paragraphs.dropFirst() {
-                let finalParagraph = "<h2>" + paragraph
-                print(finalParagraph)
-                print("______________________")
-                
-                if let attributedString = try? NSMutableAttributedString(data: Data(finalParagraph.utf8), options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
-
-                    
+                if !attributedString.string.isEmpty {
                     // Format all text
                     attributedString.addAttribute(.font, value: UIFont.bodyTextLarge, range: NSRange(location: 0, length: attributedString.string.count - 1))
-                    
-                    // Format subtitle
-                    let subtitle = finalParagraph.slice(from: "<h2>", to: "</h2>")
-                    if let subtitleRange = attributedString.string.range(of: subtitle ?? "") {
-                        let nsSubtitleRange = NSRange(subtitleRange, in: attributedString.string)
-                        attributedString.addAttribute(.font, value: UIFont.subtitle, range: nsSubtitleRange)
+
+                    // Format title
+                    let titleString = contents.slice(from: "<h1>", to: "</h1>")
+                    let formattedTitle = titleString?.replacingOccurrences(of: "&amp;", with: "&")
+                    if let titleRange = attributedString.string.range(of: formattedTitle ?? "") {
+                        let nsTitleRange = NSRange(titleRange, in: attributedString.string)
+                        attributedString.addAttribute(.font, value: UIFont.headline, range: nsTitleRange)
                     }
 
-                    mutableAttributedString.append(attributedString)
+                    mutableAttributedString = attributedString
                     mutableAttributedString.append(newLine)
+                }
+            }
+            
+            // Remaining paragraphs
+            var hasFormattedHThreeSubtitles = false
+            
+            if contents.contains("<h2>") {
+                let paragraphs = contents.components(separatedBy: "<h2>")
+                for paragraph in paragraphs.dropFirst() {
+                    let finalParagraph = "<h2>" + paragraph
+                    print(finalParagraph)
+                    print("______________________")
+                    
+                    if let attributedString = try? NSMutableAttributedString(data: Data(finalParagraph.utf8), options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
+
+                        // Format all text
+                        attributedString.addAttribute(.font, value: UIFont.bodyTextLarge, range: NSRange(location: 0, length: attributedString.string.count - 1))
+                        
+                        // Format H2 subtitle
+                        var subtitle = finalParagraph.slice(from: "<h2>", to: "</h2>")
+                        subtitle = subtitle?.replacingOccurrences(of: "    ", with: " ")
+                        if let subtitleRange = attributedString.string.range(of: subtitle ?? "") {
+                            let nsSubtitleRange = NSRange(subtitleRange, in: attributedString.string)
+                            attributedString.addAttribute(.font, value: UIFont.subtitle, range: nsSubtitleRange)
+                        }
+                        
+                        // Format H3 subtitle
+                        let paragraphsHThree = contents.components(separatedBy: "<h3>")
+                        paragraphsHThree.forEach {
+                            let formattedParagraph = "<h3>" + $0
+                            let subtitleHThree = formattedParagraph.slice(from: "<h3>", to: "</h3>")
+                            if let subtitleRange = attributedString.string.range(of: subtitleHThree ?? "") {
+                                let nsSubtitleRange = NSRange(subtitleRange, in: attributedString.string)
+                                attributedString.addAttribute(.font, value: UIFont.linkTextButtonNormal, range: nsSubtitleRange)
+                            }
+                            hasFormattedHThreeSubtitles = true
+                        }
+
+                        mutableAttributedString.append(attributedString)
+                        mutableAttributedString.append(newLine)
+                    }
+                }
+            }
+            
+            if contents.contains("<h3>") && !hasFormattedHThreeSubtitles {
+                let paragraphs = contents.components(separatedBy: "<h3>")
+                for paragraph in paragraphs {
+                    let finalParagraph = "<h3>" + paragraph
+                    if let attributedString = try? NSMutableAttributedString(data: Data(finalParagraph.utf8), options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
+                        // Format all text
+                        attributedString.addAttribute(.font, value: UIFont.bodyTextLarge, range: NSRange(location: 0, length: attributedString.string.count - 1))
+                        
+                        // Format subtitle
+                        let subtitle = finalParagraph.slice(from: "<h3>", to: "</h3>")
+                        if let subtitleRange = attributedString.string.range(of: subtitle ?? "") {
+                            let nsSubtitleRange = NSRange(subtitleRange, in: attributedString.string)
+                            attributedString.addAttribute(.font, value: UIFont.subtitle, range: nsSubtitleRange)
+                        }
+
+                        mutableAttributedString.append(attributedString)
+                        mutableAttributedString.append(newLine)
+                    }
                 }
             }
             
