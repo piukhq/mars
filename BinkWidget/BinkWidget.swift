96e8f08e26454ab2ec74a2d78eadea3cff5d8b25
Sean Williams <swilliams@bink.com>
2021-06-22 12:10:34 +0100

    Create widget target and rename objects

---
+++ BinkWidget/BinkWidget.swift
@@ -0,0 +1,70 @@
+//
+//  BinkWidget.swift
+//  BinkWidget
+//
+//  Created by Sean Williams on 22/06/2021.
+//  Copyright © 2021 Bink. All rights reserved.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct QuickLaunchProvider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate) ?? Date()
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct QuickLaunchEntryView: View {
+    var entry: QuickLaunchProvider.Entry
+
+    var body: some View {
+        
+        Text(entry.date, style: .time)
+    }
+}
+
+@main
+struct BinkWidget: Widget {
+    let kind: String = "com.bink.QuickLaunch"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: QuickLaunchProvider()) { entry in
+            QuickLaunchEntryView(entry: entry)
+        }
+        .configurationDisplayName("Bink quick launch")
+        .description("Quickly access your favourite loyalty cards.")
+        .supportedFamilies([.systemMedium])
+    }
+}
+
+struct BinkWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        QuickLaunchEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
+    }
+}
