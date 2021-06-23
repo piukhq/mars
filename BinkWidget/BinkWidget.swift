1c14127476cbee32c491ba2ba5ac794c416434e6
Sean Williams <swilliams@bink.com>
2021-06-23 17:03:37 +0100

    Add preview wallet cards. Construct add more cards cell.

--- BinkWidget/BinkWidget.swift
+++ BinkWidget/BinkWidget.swift
@@ -9,13 +9,19 @@
 import WidgetKit
 import SwiftUI
 
+let previewWalletCards: [MembershipCard] = [
+    MembershipCard(id: "1", image: UIImage(named: "hn") ?? UIImage(), backgroundColor: .black, isLight: false, cardNumber: "2678876526788762"),
+    MembershipCard(id: "2", image: UIImage(named: "wasabi") ?? UIImage(), backgroundColor: .green, isLight: true, cardNumber: "26788765267898202"),
+    MembershipCard(id: "3", image: UIImage(named: "iceland") ?? UIImage(), backgroundColor: .white, isLight: true, cardNumber: "2678876526789234")
+]
+
 struct QuickLaunchProvider: TimelineProvider {
     func placeholder(in context: Context) -> WidgetContent {
-        WidgetContent(date: Date(), hasCurrentUser: true)
+        WidgetContent(date: Date(), hasCurrentUser: true, walletCards: previewWalletCards)
     }
 
     func getSnapshot(in context: Context, completion: @escaping (WidgetContent) -> Void) {
-        let entry = WidgetContent(date: Date(), hasCurrentUser: true)
+        let entry = WidgetContent(date: Date(), hasCurrentUser: true, walletCards: previewWalletCards)
         completion(entry)
     }
 
@@ -27,7 +33,7 @@ struct QuickLaunchProvider: TimelineProvider {
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .second, value: hourOffset, to: currentDate) ?? Date()
-            let entry = WidgetContent(date: entryDate, hasCurrentUser: hasCurrentUser)
+            let entry = WidgetContent(date: entryDate, hasCurrentUser: hasCurrentUser, walletCards: previewWalletCards)
             entries.append(entry)
         }
 
@@ -39,44 +45,56 @@ struct QuickLaunchProvider: TimelineProvider {
 struct WidgetContent: TimelineEntry {
     let date: Date
     let hasCurrentUser: Bool
-    let walletCards: [MembershipCard] = []
+    let walletCards: [MembershipCard]
 }
 
-struct MembershipCard {
+struct MembershipCard: Hashable {
+    let id: String
     let image: UIImage
-    let backGroundColor: UIColor
+    let backgroundColor: Color
+    let isLight: Bool
     let cardNumber: String
 }
 
 struct QuickLaunchEntryView: View {
     let model: QuickLaunchProvider.Entry
     var twoColumnGrid = [GridItem(.flexible()), GridItem(.flexible())]
-    var symbols = ["keyboard", "hifispeaker.fill", "printer.fill", "tv.fill"]
     
     var body: some View {
         if model.hasCurrentUser {
             LazyVGrid(columns: twoColumnGrid, spacing: 33) {
-                ForEach(symbols, id: \.self) { _ in
+                ForEach(model.walletCards, id: \.self) { membershipCard in
                     HStack(alignment: .center, spacing: 42.0) {
-                        Image("hn")
+                        Image(uiImage: membershipCard.image)
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .padding(.leading, 3.0)
                             .frame(width: 36.0, height: 36.0)
-                        Text("••5776")
-                            .font(.footnote)
+                        Text("••\(String(membershipCard.cardNumber.suffix(4)))")
+                            .font(.system(size: 10, weight: .semibold, design: .rounded))
                             .fontWeight(.semibold)
-                            .foregroundColor(Color.white)
+                            .foregroundColor(membershipCard.isLight ? .black : .white)
                     }
                     .background(
                         RoundedRectangle(cornerRadius: 14.0)
-                            .frame(minWidth: 153, maxWidth: .infinity, minHeight: 65)
-                            .foregroundColor(.black)
+                            .frame(minWidth: 153, maxWidth: .infinity, minHeight: 64)
+                            .foregroundColor(membershipCard.backgroundColor)
                     )
                 }
+                
+                if model.walletCards.count < 4 {
+                    Image(systemName: "plus")
+                        .foregroundColor(.white)
+                        .background(
+                            RoundedRectangle(cornerRadius: 14)
+                                .frame(minWidth: 153, maxWidth: .infinity, minHeight: 64)
+                                .foregroundColor(.clear)
+                                .border(Color.gray, width: 1).cornerRadius(14)
+                        )
+                }
             }
-            .frame(height: 160)
-            .padding(.all, 9.0)
+            .frame(height: 160.0)
+            .padding(.all, 11.0)
             .background(Color("WidgetBackground"))
         } else {
             UnauthenticatedSwiftUIView()
@@ -100,7 +118,7 @@ struct BinkWidget: Widget {
 
 struct BinkWidget_Previews: PreviewProvider {
     static var previews: some View {
-        QuickLaunchEntryView(model: WidgetContent(date: Date(), hasCurrentUser: true))
+        QuickLaunchEntryView(model: WidgetContent(date: Date(), hasCurrentUser: true, walletCards: previewWalletCards))
             .previewContext(WidgetPreviewContext(family: .systemMedium))
             .preferredColorScheme(.light)
     }
