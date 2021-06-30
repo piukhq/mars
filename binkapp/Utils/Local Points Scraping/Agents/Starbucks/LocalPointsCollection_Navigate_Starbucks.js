aa69c706e22a2681b5c2a579522e1f55796c270b
Nick Farrant <nfarrant@bink.com>
2021-06-30 16:17:32 +0100

    Agent file

---
+++ binkapp/Utils/Local Points Scraping/Agents/Starbucks/LocalPointsCollection_Navigate_Starbucks.js
@@ -0,0 +1,81 @@
+var username = "%@"
+var password = "%@"
+
+var formQuery = "form#sign-in-form"
+var usernameInputQuery = "form#login-form input[type=email]"
+var passwordInputQuery = "form#login-form input[type=password]"
+var submitButtonQuery = "form#login-form button[type=submit]"
+
+var pointsValueQuery = "#pointSummary [class*=Points] [class*=Points]"
+
+var incorrectCredentialsQuery = "p.ui-component__notice__error-text"
+
+handleNavigation()
+
+function handleNavigation() {
+    // If we can identify the points value, scrape
+
+    var pts = document.querySelector(pointsValueQuery)
+
+    if (pts) {
+        var num = pts.innerHTML.match(/\d+/);
+        return {
+            "points": num[0]
+        }
+    }
+
+    window.location.href = "https://www.google.com"
+
+    return {}
+
+    // If we can't identify a points value, can we identify one of the following:
+    // Recaptcha, incorrect credentials message
+
+    var error = document.querySelector(incorrectCredentialsQuery)
+    if (error && error.innerHTML !== "") {
+        return {
+            "did_attempt_login": true,
+            "error_message": "Login failed. Incorrect credentials identified: " + error.innerHTML
+        }
+    }
+
+    // If we can identify the login form, login
+
+    var f = document.querySelector(formQuery)
+    if (f) {
+        // Override the form's id to enable easier querying
+        f.id = "login-form"
+
+        var u = document.querySelector(usernameInputQuery)
+        if (!u) {
+            return {
+                "error_message": "Login failed. Email/username input field could not be identified."
+            }
+        }
+        u.value = username
+
+        var p = document.querySelector(passwordInputQuery)
+        if (!p) {
+            return {
+                "error_message": "Login failed. Password input field could not be identified."
+            }
+        }
+        p.value = password
+
+        var b = Array.from(document.querySelectorAll('form#login-form button')).filter(el => el.type === "submit")[0]
+        if (!b) {
+            return {
+                "error_message": "Login failed. Submit button could not be identified."
+            }
+        }
+        b.click()
+
+        return {
+            "did_attempt_login": true
+        }
+    }
+
+    // If we cannot identify a login form, points value, recaptcha or incorrect credentials
+    // We should assume the client is redirecting, and the idle timer should handle moving to retry state
+    return {}
+}
