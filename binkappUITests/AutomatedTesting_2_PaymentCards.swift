1b9d43c96f143712704694c3bbdae917369785df
Sean Williams <swilliams@bink.com>
2021-07-06 16:22:25 +0100

    Write test to check successful PLL linkage

--- binkappUITests/AutomatedTesting_2_PaymentCards.swift
+++ binkappUITests/AutomatedTesting_2_PaymentCards.swift
@@ -53,4 +53,31 @@ class AutomatedTesting_2_PaymentCards: XCTestCase {
         
         XCTAssertTrue(app.staticTexts["B Testerson"].waitForExistence(timeout: 10))
     }
+    
+    func test_1_PLL_link_isSuccessful() {
+        sleep(1) // <<<<<<<<<<<<<<<<<< change to 10
+        app.buttons["Browse brands"].tap()
+        sleep(1)
+        app.tables.cells["Iceland"].tap()
+        sleep(1)
+        app.buttons["Add my card"].tap()
+        let cardNumberTextfield = app.textFields["Bonus card number"]
+        cardNumberTextfield.tap()
+        cardNumberTextfield.typeText("6332040000200000002")
+        app.buttons["next"].tap()
+        app.typeText("one")
+        app.buttons["next"].tap()
+        app.typeText("rg1 1aa")
+
+        app.toolbars["Toolbar"].buttons["Done"].tap()
+        app.buttons["Add card"].tap()
+        app.buttons["Done"].tap()
+        
+        sleep(1)
+        
+        app.buttons["Payment"].tap()
+        app.collectionViews.cells["B Testerson"].tap()
+        
+        XCTAssertTrue(app.staticTexts["Linked to 1 loyalty card"].waitForExistence(timeout: 10))
+    }
 }
