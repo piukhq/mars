a2484568125ae491f134377f5d3e14557f7085bf
Sean Williams <swilliams@bink.com>
2021-07-30 09:17:36 +0100

    Add http/s to queriesSchemes. Set form textview to clear.

--- binkapp/Screens/BinkUI/Form/BaseFormViewController.swift
+++ binkapp/Screens/BinkUI/Form/BaseFormViewController.swift
@@ -71,6 +71,7 @@ class BaseFormViewController: BinkViewController, Form {
         textView.isScrollEnabled = false
         textView.isUserInteractionEnabled = true
         textView.isEditable = false
+        textView.backgroundColor = .clear
         textView.textContainerInset = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: -5)
         textView.linkTextAttributes = [.foregroundColor: UIColor.blueAccent, .underlineStyle: NSUnderlineStyle.single.rawValue]
         return textView
