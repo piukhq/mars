d5aca1653be532b2d6cdf44ad8fb1c5314a8cd6a
Sean Williams <swilliams@bink.com>
2021-08-10 15:03:44 +0100

    Use widget(url) modifier for sign_in and from_edge

--- BinkWidget/QuickLaunch/BinkWidget.swift
+++ BinkWidget/QuickLaunch/BinkWidget.swift
@@ -73,7 +73,7 @@ struct QuickLaunchEntryView: View {
 
     var twoColumnGrid = [GridItem(.flexible(), alignment: .topLeading), GridItem(.flexible())]
     let hasCurrentUser = UserDefaults(suiteName: WidgetType.quickLaunch.userDefaultsSuiteID)?.bool(forKey: "hasCurrentUser") ?? false
-    let url = URL(string: "quicklaunch-widget://from_edge") ?? URL(string: "quicklaunch-widget://")!
+    let url = URL(string: "quicklaunch-widget://from_edge")
     
     enum Constants {
         static let widgetHeight: CGFloat = 164
--- BinkWidget/QuickLaunch/UnauthenticatedSwiftUIView.swift
+++ BinkWidget/QuickLaunch/UnauthenticatedSwiftUIView.swift
@@ -16,10 +16,7 @@ struct UnauthenticatedSwiftUIView: View {
         static let TextSize: CGFloat = 12.0
     }
     
-    let url = URL(string: "quicklaunch-widget://sign_in") ?? URL(string: "quicklaunch-widget://")!
-    
     var body: some View {
-        Link(destination: url) {
             HStack(alignment: .center, spacing: 0) {
                 Image("bink-logo")
                     .resizable()
@@ -34,6 +31,6 @@ struct UnauthenticatedSwiftUIView: View {
             }
             .padding(.vertical, Constants.VerticalPadding)
             .background(Color("UnAuthWidgetBackground"))
-        }
+            .widgetURL(URL(string: "quicklaunch-widget://sign_in"))
     }
 }
