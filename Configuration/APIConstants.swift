5aa255f47d74142d8991c86aad20182fe1b83026
Sean Williams <swilliams@bink.com>
2021-08-24 09:31:11 +0100

    Change Debug menu key/value to debug

--- Configuration/APIConstants.swift
+++ Configuration/APIConstants.swift
@@ -43,7 +43,7 @@ enum Configuration {
     enum ConfigurationKey: String {
         case apiBaseUrl = "API_BASE_URL"
         case secret = "SECRET"
-        case debugMenu = "DEBUG_MENU"
+        case debug = "DEBUG"
     }
     
     static func value(for key: ConfigurationKey) throws -> String {
@@ -53,7 +53,7 @@ enum Configuration {
     }
     
     static func isDebug() -> Bool {
-        if let _ = try? value(for: .debugMenu) {
+        if let _ = try? value(for: .debug) {
             return true
         }
         return false
@@ -77,7 +77,7 @@ enum APIConstants {
     }
 
     static var baseURLString: String {
-        if let _ = try? Configuration.value(for: .debugMenu) {
+        if let _ = try? Configuration.value(for: .debug) {
             // If we have set the environment from the debug menu
             if let debugBaseURLString = Current.userDefaults.value(forDefaultsKey: .debugBaseURL) as? String {
                 return debugBaseURLString
