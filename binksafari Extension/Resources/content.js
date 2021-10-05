e840dbb9a398b3497d8f6e7df6f9b6aeadd737fa
Nick Farrant <nfarrant@bink.com>
2021-10-05 15:05:42 +0100

    POC complete

--- binksafari Extension/Resources/content.js
+++ binksafari Extension/Resources/content.js
@@ -1,7 +1,7 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
-
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+var promoCodeInput = document.querySelector('input#dwfrm_cart_couponCode');
+var applyButton = document.querySelector('button#add-coupon');
+if (promoCodeInput && applyButton) {
+    promoCodeInput.value = "FF0001ABC";
+    applyButton.click();
+    alert("You have a FatFace promotion code in Bink. We'ven tried applying it to your basket.");
+}
