18ec0a9b1578ea03f8a0c463304ebac34a1e8f7c
Sean Williams <swilliams@bink.com>
2021-10-25 15:27:49 +0100

    Use constants enum. Implement animation.

--- binkapp/Screens/AccessForms/Login/CheckYourInbox/CheckYourInboxViewController.swift
+++ binkapp/Screens/AccessForms/Login/CheckYourInbox/CheckYourInboxViewController.swift
@@ -6,15 +6,24 @@
 //  Copyright © 2021 Bink. All rights reserved.
 //
 
+import Lottie
 import UIKit
 
 class CheckYourInboxViewController: BinkViewController {
+    enum Constants {
+        static let animationDimension: CGFloat = 330
+        static let horizontalInset: CGFloat = 25
+        static let bottomInset: CGFloat = 140
+        static let buttonFadeInDelay: CGFloat = 3
+        static let buttonFadeInAnimationDuration: CGFloat = 0.5
+    }
+    
     private lazy var stackScrollView: StackScrollView = {
-        let stackView = StackScrollView(axis: .vertical, arrangedSubviews: [textView], adjustForKeyboard: true)
+        let stackView = StackScrollView(axis: .vertical, arrangedSubviews: [textView, animationView], adjustForKeyboard: true)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.backgroundColor = .clear
-        stackView.margin = UIEdgeInsets(top: 0, left: 25, bottom: 0, right: 25)
-        stackView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 140, right: 0)
+        stackView.margin = UIEdgeInsets(top: 0, left: Constants.horizontalInset, bottom: 0, right: Constants.horizontalInset)
+        stackView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: Constants.bottomInset, right: 0)
         stackView.distribution = .fill
         stackView.alignment = .fill
         view.addSubview(stackView)
@@ -25,10 +34,20 @@ class CheckYourInboxViewController: BinkViewController {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.isScrollEnabled = false
-        textView.isUserInteractionEnabled = false 
+        textView.isUserInteractionEnabled = false
         textView.isEditable = false
         textView.backgroundColor = .clear
         return textView
+    }()
+    
+    private lazy var animationView: AnimationView = {
+        let animationView = AnimationView(name: "Paperplane")
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.heightAnchor.constraint(equalToConstant: Constants.animationDimension).isActive = true
+        animationView.widthAnchor.constraint(equalToConstant: Constants.animationDimension).isActive = true
+        return animationView
     }()
     
     private var viewModel: CheckYourInboxViewModel
@@ -44,10 +63,9 @@ class CheckYourInboxViewController: BinkViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        textView.delegate = self
         configureUI()
         configureButton()
+        animationView.play()
     }
 
     override func configureForCurrentTheme() {
@@ -56,7 +74,7 @@ class CheckYourInboxViewController: BinkViewController {
     }
     
     private func configureUI() {
-        textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 140, right: 0)
+        textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: Constants.bottomInset, right: 0)
         textView.attributedText = viewModel.text
         textView.linkTextAttributes = [.foregroundColor: UIColor.blueAccent, .underlineStyle: NSUnderlineStyle.single.rawValue]
         
@@ -70,11 +88,14 @@ class CheckYourInboxViewController: BinkViewController {
     
     private func configureButton() {
         if let primaryTitle = viewModel.primaryButtonTitle, let primaryAction = viewModel.primaryButtonAction {
-            footerButtons.append(BinkButton(type: .gradient, title: primaryTitle, action: primaryAction))
+            let button = BinkButton(type: .gradient, title: primaryTitle, action: primaryAction)
+            button.setAlpha(0)
+            DispatchQueue.main.asyncAfter(deadline: .now() + Constants.buttonFadeInDelay) {
+                UIView.animate(withDuration: Constants.buttonFadeInAnimationDuration, delay: 0, options: .curveEaseInOut) {
+                    button.setAlpha(1)
+                }
+            }
+            footerButtons.append(button)
         }
     }
-}
-
-extension CheckYourInboxViewController: UITextViewDelegate {
-    
 }
