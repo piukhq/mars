e714cecead45edda36c84c0dc11af01bcef19622
Sean Williams <swilliams@bink.com>
2022-07-29 09:37:42 +0100

    Add button and stackview

--- binkapp/Screens/BinkUI/InfoAlertView.swift
+++ binkapp/Screens/BinkUI/InfoAlertView.swift
@@ -63,13 +63,33 @@ class InfoAlertView: UIView {
     private var timer: Timer?
     
     private lazy var textLabel: UILabel = {
-        let label = UILabel(frame: CGRect(x: self.bounds.origin.x + 20, y: self.bounds.origin.y, width: self.bounds.width - 40, height: self.bounds.height))
+        let label = UILabel()
         label.textAlignment = .left
         label.textColor = .white
         label.font = .alertText
         label.numberOfLines = 2
         label.lineBreakMode = .byTruncatingTail
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Action", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .buttonText
+        button.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        return button
+    }()
+    
+    private lazy var stackview: UIStackView = {
+        let stackview = UIStackView(arrangedSubviews: [textLabel, button])
+        stackview.frame = CGRect(x: self.bounds.origin.x + 20, y: self.bounds.origin.y, width: self.bounds.width - 40, height: self.bounds.height)
+        stackview.axis = .horizontal
+        stackview.distribution = .fill
+        stackview.spacing = 15
+        return stackview
     }()
     
     init(message: String, type: AlertType, window: UIWindow) {
@@ -89,6 +109,12 @@ class InfoAlertView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 8
+        layer.cornerCurve = .continuous
+        addSubview(stackview)
     }
     
     static func show(_ message: String, type: AlertType) {
@@ -154,13 +180,7 @@ class InfoAlertView: UIView {
         })
         
         timer = Timer.scheduledTimer(withTimeInterval: type.timeInterval, repeats: false) { _ in
-            self.hide()
+//            self.hide()
         }
-    }
-    
-    private func configure() {
-        addSubview(textLabel)
-        layer.cornerRadius = 8
-        layer.cornerCurve = .continuous
     }
 }
--- binkapp/SwiftUI Views/DebugMenuView.swift
+++ binkapp/SwiftUI Views/DebugMenuView.swift
@@ -224,7 +224,7 @@ struct PickerDebugRow: View {
                 case .long:
                     InfoAlertView.show("This is a long snackbar", type: .snackbar(.long))
                 case .multiline:
-                    InfoAlertView.show("This is a snackbar with so much text, the label is like whuuuut?! I can't contain this much text yo, I'm gonna spill", type: .snackbar(.short))
+                    InfoAlertView.show("This is a snackbar with so much text, the label is like whuuuut?! I can't contain this much text yo, I'm gonna spill", type: .snackbar(.long))
                 case .action:
                     InfoAlertView.show("This is a snackbar with an action", type: .snackbar(.long))
                 case .input:
