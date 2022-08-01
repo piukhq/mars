a6af76bc43b8328a322418fee26091d5c07514c2
Ricardo Silva <risilva10223@gmail.com>
2022-08-01 10:07:48 +0100

    - keeping code in one line

--- binkapp/Screens/BinkUI/Buttons/BinkButtonsView.swift
+++ binkapp/Screens/BinkUI/Buttons/BinkButtonsView.swift
@@ -102,9 +102,7 @@ extension BinkButtonsView {
     static let bottomPadding: CGFloat = 16
     static let buttonSpacing: CGFloat = 25
     static let bottomSafePadding: CGFloat = {
-        let window = UIApplication.shared.connectedScenes
-            .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }
-            .first { $0.isKeyWindow }
+        let window = UIApplication.shared.connectedScenes.flatMap { ($0 as? UIWindowScene)?.windows ?? [] }.first { $0.isKeyWindow }
         let safeAreaBottom = window?.safeAreaInsets.bottom ?? 0
         return bottomPadding + safeAreaBottom
     }()
