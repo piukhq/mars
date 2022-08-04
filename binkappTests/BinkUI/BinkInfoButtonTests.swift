d75abbad8e276e2d6506f9919c8762099e97550c
Sean Williams <swilliams@bink.com>
2022-08-04 16:30:13 +0100

    Remove unnecessary tests

--- binkappTests/BinkUI/BinkInfoButtonTests.swift
+++ binkappTests/BinkUI/BinkInfoButtonTests.swift
@@ -17,18 +17,6 @@ class BinkInfoButtonTests: XCTestCase, CoreDataTestable {
         sut = BinkInfoButton(type: .custom)
     }
     
-    func test_imageEdgeInsets_returnCorrectValues() {
-        XCTAssertEqual(sut.imageEdgeInsets, UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
-        sut.imageView?.image = Asset.iconCheck.image
-        XCTAssertEqual(sut.imageEdgeInsets, LayoutHelper.BinkInfoButton.imageEdgeInsets)
-    }
-    
-    func test_titleEdgeInsets_returnCorrectValues() {
-        XCTAssertEqual(sut.titleEdgeInsets, UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
-        sut.imageView?.image = Asset.iconCheck.image
-        XCTAssertEqual(sut.titleEdgeInsets, LayoutHelper.BinkInfoButton.titleEdgeInsets)
-    }
-    
     func test_accessibilityIdentifier_returnCorrectValue() {
         sut.imageView?.image = Asset.iconCheck.image
         XCTAssertEqual(sut.accessibilityIdentifier, "Bink info button")
