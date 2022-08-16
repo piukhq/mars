270bcbfa30e37da577653e48780aae4706200142
Sean Williams <swilliams@bink.com>
2022-08-16 13:27:57 +0100

    Save logs to documents

--- binkapp/BinkNetworking/BinkNetworkingLogger.swift
+++ binkapp/BinkNetworking/BinkNetworkingLogger.swift
@@ -10,23 +10,9 @@ import Foundation
 import Alamofire
 
 class BinkNetworkingLogger: EventMonitor {
-    private var log: BinkNetworkingLog!
-    
-//    func requestDidResume(_ request: Request) {
-//        let body = request.request.flatMap { $0.httpBody.map { String(decoding: $0, as: UTF8.self) } } ?? "None"
-//        let message = """
-//        ⚡️ Request Started: \(request)
-//        ⚡️ Body Data: \(body)
-//        ⚡️ Base URL: \(APIConstants.baseURLString)
-//        """
-//
-//        let endpointString = "Request Started: \(request)"
-////        debugPrint("SW: \(message)")
-//
-//        log = BinkNetworkingLog(endpoint: endpointString, baseURL: APIConstants.baseURLString, body: body)
-////        debugPrint("SW: \(log)")
-//    }
-    
+    private var logs: [BinkNetworkingLog] = []
+    private var binkLog: BinkLog!
+
     func request(_ request: DataRequest, didParseResponse response: DataResponse<Data?, AFError>) {
         guard
             let task = request.task,
@@ -39,41 +25,61 @@ class BinkNetworkingLogger: EventMonitor {
         }
         
         //: - Request
-        let requestBody = urlRequest.urlRequest.flatMap { $0.httpBody.map { String(decoding: $0, as: UTF8.self) } } ?? "None"
-        print("SW:⚡️ \(httpMethod) '\(requestURL.absoluteString)'")
-        print("SW:⚡️ Base URL: \(APIConstants.baseURLString)")
-        print("SW:⚡️ Request Body: \(requestBody)")
-        
+        let requestBodyFormatted = urlRequest.urlRequest.flatMap { $0.httpBody.map { String(decoding: $0, as: UTF8.self) } } ?? "None"
+        let endpoint = "\(httpMethod) '\(requestURL.absoluteString)'"
+        let baseURL = "\(APIConstants.baseURLString)"
+        let requestBody = "\(requestBodyFormatted)"
+        print("\(httpMethod) '\(requestURL.absoluteString)'")
+        print("\(APIConstants.baseURLString)")
+        print("\(requestBody)")
         
         //: - Response
         let elapsedTime = metrics.taskInterval.duration
         
         guard let response = response.response else { return }
 
-        if let error = task.error {
-            print("SW:⚡️ [Error] \(httpMethod) '\(requestURL.absoluteString)' [\(String(format: "%.04f", elapsedTime)) s]:")
-            print(error)
-        } else {
-            print("SW:⚡️ HTTP Status Code: \(response.statusCode)")
-            print("SW:⚡️ Response Time: [\(String(format: "%.04f", elapsedTime)) s]")
-            
-            guard let data = request.data else { return }
-            
-            do {
-                let jsonObject = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
-                let prettyData = try JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted)
-                
-                if let prettyString = String(data: prettyData, encoding: .utf8) {
-                    print("SW:⚡️ Response Body: \(prettyString)")
-                }
-            } catch {
-                if let errorString = String(data: data, encoding: .utf8) {
-                    print("SW:⚡️ Response Body: \(errorString)")
-                }
+        let statusCode = "\(response.statusCode)"
+        let responseTime = "[\(String(format: "%.04f", elapsedTime)) s]"
+        print("SW:⚡️ HTTP Status Code: \(response.statusCode)")
+        print("SW:⚡️ Response Time: [\(String(format: "%.04f", elapsedTime)) s]")
+        
+        guard let data = request.data else { return }
+        var responseBody = ""
+
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            let prettyData = try JSONSerialization.data(withJSONObject: jsonObject, options: .withoutEscapingSlashes)
+
+            if let prettyString = String(data: prettyData, encoding: .utf8) {
+                responseBody = prettyString
+                print("SW:⚡️ Response Body: \(prettyString)")
+            }
+        } catch {
+            if let errorString = String(data: data, encoding: .utf8) {
+                responseBody = errorString
+                print("SW:⚡️ Response Body: \(errorString)")
             }
         }
-        
+
         logDivider()
+        
+        let log = BinkNetworkingLog(endpoint: endpoint, baseURL: baseURL, requestBody: requestBody, httpStatusCode: statusCode, responseTime: responseTime, responseBody: responseBody)
+        logs.append(log)
+        saveLogs()
+    }
+    
+    private func saveLogs() {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        guard let encoded = try? encoder.encode(logs) else { return }
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        guard let path = documents?.appendingPathComponent("/networkLogs.nal") else { return }
+        
+        do {
+            try encoded.write(to: path, options: .atomicWrite)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
     private func logDivider() {
@@ -81,8 +87,15 @@ class BinkNetworkingLogger: EventMonitor {
     }
 }
 
-struct BinkNetworkingLog {
+struct BinkNetworkingLog: Codable {
     let endpoint: String
     let baseURL: String
+    let requestBody: String
+    let httpStatusCode: String
+    let responseTime: String
+    let responseBody: String
+}
+
+struct BinkLog: Codable {
     let body: String
 }
