31c46d259748bfd87fce0c65ef64b9c0d8443136
Ricardo Silva <risilva10223@gmail.com>
2023-03-29 10:09:35 +0100

    - animation test

--- binkapp/Screens/Polling/PollSwiftUIView.swift
+++ binkapp/Screens/Polling/PollSwiftUIView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct PollSwiftUIView: View {
     @ObservedObject var viewModel: PollSwiftUIViewModel
-    
+    @State private var drawingWidth = false
     var body: some View {
         VStack {
             Text(viewModel.pollData?.title ?? "")
@@ -21,33 +21,36 @@ struct PollSwiftUIView: View {
             .frame(height: 100)
             .background(.teal)
             
-            ScrollView {
-                VStack {
-                    GeometryReader { gp in
-                        
-                    }
-                    ForEach(viewModel.pollData?.answers ?? [], id: \.self) { answer in
-                        Button(action: {
-                            // What to do
-                        }) {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                    .fill(.white)
-                                    //.frame(maxWidth: .infinity)
-                                    .frame(height: 60)
-//                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-//                                    .fill(.blue)
-//                                    .frame(maxWidth: .infinity)
-//                                    .frame(height: 60)
-//                                    .animation(.easeInOut(duration: 10).repeatForever(autoreverses: false), value: drawingWidth)
-                                
-                                Text(answer)
+            GeometryReader { gp in
+                ScrollView {
+                    VStack {
+                        ForEach(viewModel.pollData?.answers ?? [], id: \.self) { answer in
+                            Button(action: {
+                                // What to do
+                            }) {
+                                ZStack(alignment: .leading) {
+                                    RoundedRectangle(cornerRadius: 12, style: .continuous)
+                                        .fill(.white)
+                                        .frame(maxWidth: gp.size.width)
+                                        .frame(height: 60)
+                                    RoundedRectangle(cornerRadius: 12, style: .continuous)
+                                        .fill(.blue)
+                                        .frame(maxWidth: drawingWidth ? gp.size.width * 1.0 : 0, alignment: .leading)
+                                        .frame(height: 60, alignment: .leading)
+                                        .animation(.easeInOut(duration: 2).repeatForever(autoreverses: false), value: drawingWidth)
+                                    
+                                    Text(answer)
+                                        .frame(maxWidth: .infinity, alignment: .center)
+                                }
+                                .padding(.top, 20)
                             }
-                            .padding(.top, 20)
                         }
                     }
+                    .padding()
+                    .onAppear {
+                        drawingWidth.toggle()
+                    }
                 }
-                .padding()
             }
             
             VStack(spacing: 6) {
